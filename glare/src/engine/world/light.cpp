d46be55bb2ec86a3c64b406697f2ce7d5f183e76
Anthony Diamond <sonickid8@live.com>
2021-02-07 20:36:58 -0700

    Lights now report the correct entity type.

--- glare/src/engine/world/light.cpp
+++ glare/src/engine/world/light.cpp
@@ -5,7 +5,7 @@ namespace engine
 {
 	Entity create_light(World& world, const math::Vector& position, const graphics::ColorRGB& color, LightType type, Entity parent)
 	{
-		auto light = create_pivot(world, position, parent);
+		auto light = create_pivot(world, position, parent, EntityType::Light);
 
 		world.get_registry().emplace<LightComponent>(light, type, color);
 
