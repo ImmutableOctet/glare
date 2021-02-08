ca9bed8f41b30fcda39d4b7cc38a48cf34caea2f
Anthony Diamond <sonickid8@live.com>
2021-02-07 20:23:10 -0700

    Added orthographic projection as an option.

--- glare/src/engine/world/camera.cpp
+++ glare/src/engine/world/camera.cpp
@@ -3,16 +3,30 @@
 #include <math/math.hpp>
 #include <engine/world/world.hpp>
 
+#include <util/string.hpp>
+
 namespace engine
 {
-	CameraParameters::CameraParameters(float v_fov_deg, float near_plane, float far_plane, float aspect_ratio)
-		: fov(glm::radians(v_fov_deg)), near_plane(near_plane), far_plane(far_plane), aspect_ratio(aspect_ratio) {}
+	CameraParameters::Projection CameraParameters::resolve_projection_mode(const std::string& mode)
+	{
+		auto m = util::lowercase(mode);
+
+		if (m.starts_with("ortho"))
+		{
+			return Projection::Orthographic;
+		}
+
+		return Projection::Perspective;
+	}
+
+	CameraParameters::CameraParameters(float v_fov_deg, float near_plane, float far_plane, float aspect_ratio, CameraParameters::Projection projection_mode, bool free_rotation)
+		: fov(glm::radians(v_fov_deg)), near_plane(near_plane), far_plane(far_plane), aspect_ratio(aspect_ratio), projection_mode(projection_mode), free_rotation(free_rotation) {}
 
 	Entity create_camera(World& world, CameraParameters params, Entity parent, bool make_active)
 	{
 		auto& registry = world.get_registry();
 		
-		auto entity = create_entity(world, parent);
+		auto entity = create_entity(world, parent, EntityType::Camera);
 
 		registry.emplace<CameraParameters>(entity, params);
 
--- glare/src/engine/world/camera.hpp
+++ glare/src/engine/world/camera.hpp
@@ -6,12 +6,24 @@
 
 namespace engine
 {
+	enum class CameraProjection : std::uint8_t
+	{
+		Perspective,
+		Orthographic,
+
+		Default = Perspective
+	};
+
 	struct CameraParameters // Component type.
 	{
+		using Projection = CameraProjection;
+
 		inline static constexpr float calculate_aspect_ratio(int width, int height)
 		{
 			return (static_cast<float>(width) / static_cast<float>(height));
 		}
+
+		static Projection resolve_projection_mode(const std::string& mode);
 
 		static constexpr float NEAR_PLANE = 0.1f;
 		static constexpr float FAR_PLANE  = 4000.0f;
@@ -19,12 +31,18 @@ namespace engine
 
 		static constexpr float DEFAULT_FOV = 75.0f;
 
+		static constexpr bool DEFAULT_FREE_ROTATION = true; // false;
+
 		float fov; // Vertical FOV (Radians)
 		float aspect_ratio;
 		float near_plane;
 		float far_plane;
 
-		CameraParameters(float v_fov_deg=DEFAULT_FOV, float near_plane=NEAR_PLANE, float far_plane=FAR_PLANE, float aspect_ratio=ASPECT);
+		Projection projection_mode;
+
+		bool free_rotation;
+
+		CameraParameters(float v_fov_deg=DEFAULT_FOV, float near_plane=NEAR_PLANE, float far_plane=FAR_PLANE, float aspect_ratio=ASPECT, Projection projection_mode=Projection::Default, bool free_rotation=DEFAULT_FREE_ROTATION);
 
 		inline auto update_aspect_ratio(int width, int height)
 		{
