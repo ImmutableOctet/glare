cb26d83f5e4cbebb3ee06bbca9a73fea3bc0836d
Anthony Diamond <sonickid8@live.com>
2021-04-14 04:08:33 -0600

    Implemented debug configuration for 'light' entities.

--- glare/src/engine/world/light.cpp
+++ glare/src/engine/world/light.cpp
@@ -1,4 +1,5 @@
 #include "light.hpp"
+
 #include <engine/world/world.hpp>
 
 #include <engine/resource_manager.hpp>
@@ -6,22 +7,32 @@
 
 #include <engine/model_component.hpp>
 
+//#include <graphics/shader.hpp>
+
 namespace engine
 {
-	Entity create_light(World& world, const math::Vector& position, const graphics::ColorRGB& color, LightType type, Entity parent)
+	Entity create_light(World& world, const math::Vector& position, const graphics::ColorRGB& color, LightType type, Entity parent, bool debug_mode, pass_ref<graphics::Shader> debug_shader)
 	{
 		const auto& res = world.get_resource_manager();
 
-		//auto light = create_pivot(world, position, parent, EntityType::Light);
-		auto light = load_model(world, "assets/geometry/cube.b3d", parent, EntityType::Geometry, false, 0.0f, std::nullopt, std::nullopt, std::nullopt, res.get_shader("assets/shaders/light_box.vert", "assets/shaders/light_box.frag"));
+		Entity light = null;
 
-		auto t = world.set_position(light, position);
-		t.set_scale(4.0f);
+		if (debug_mode) // (debug_shader)
+		{
+			light = load_model(world, "assets/geometry/cube.b3d", parent, EntityType::Light, false, 0.0f, std::nullopt, std::nullopt, std::nullopt, debug_shader); // {}
 
-		auto& registry = world.get_registry();
-		auto& model = registry.get<ModelComponent>(light);
+			auto t = world.set_position(light, position);
+			t.set_scale(4.0f);
 
-		model.color = { color, 1.0f };
+			auto& registry = world.get_registry();
+			auto& model = registry.get<ModelComponent>(light);
+
+			model.color = { color, 1.0f };
+		}
+		else
+		{
+			light = create_pivot(world, position, parent, EntityType::Light);
+		}
 
 		world.get_registry().emplace<LightComponent>(light, type, color);
 
