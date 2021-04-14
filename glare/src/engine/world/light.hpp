9193a2a9b6f4b3efe3eb69b87614e897c2c24599
Anthony Diamond <sonickid8@live.com>
2021-04-14 04:08:33 -0600

    Implemented debug configuration for 'light' entities.

--- glare/src/engine/world/light.hpp
+++ glare/src/engine/world/light.hpp
@@ -3,6 +3,11 @@
 #include <engine/world/entity.hpp>
 #include <graphics/types.hpp>
 
+namespace graphics
+{
+	class Shader;
+}
+
 namespace engine
 {
 	struct LightComponent
@@ -12,5 +17,5 @@ namespace engine
 		graphics::ColorRGB color = { 1.0f, 1.0f, 1.0f };
 	};
 
-	Entity create_light(World& world, const math::Vector& position, const graphics::ColorRGB& color, LightType type=LightType::Point, Entity parent=null);
+	Entity create_light(World& world, const math::Vector& position, const graphics::ColorRGB& color, LightType type=LightType::Point, Entity parent=null, bool debug_mode=false, pass_ref<graphics::Shader> debug_shader={});
 }
