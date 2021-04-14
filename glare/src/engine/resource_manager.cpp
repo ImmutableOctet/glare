02b6a70af2ad1ecdaf1e9c448af4ef461ebb8e99
Anthony Diamond <sonickid8@live.com>
2021-04-14 00:14:41 -0600

    Implemented initial support for shaders in the 'ResourceManager' class.

--- glare/src/engine/resource_manager.cpp
+++ glare/src/engine/resource_manager.cpp
@@ -1,11 +1,15 @@
 #include "resource_manager.hpp"
 
 #include <graphics/model.hpp>
+#include <graphics/shader.hpp>
 
 #include <bullet/BulletCollision/btBulletCollisionCommon.h>
 
 #include <memory>
 #include <tuple>
+
+#include <util/string.hpp>
+#include <util/algorithm.hpp>
 
 /*
 #define AI_CONFIG_PP_PTV_NORMALIZE   "PP_PTV_NORMALIZE"
@@ -33,9 +37,15 @@ namespace engine
 
 	ResourceManager::~ResourceManager() {}
 
-	ResourceManager::ModelData ResourceManager::load_model(const std::string& path, bool load_collision, bool optimize_collision, bool force_reload) const
+	ResourceManager::ModelData ResourceManager::load_model(const std::string& path, bool load_collision, pass_ref<graphics::Shader> shader, bool optimize_collision, bool force_reload, bool cache_result) const
 	{
 		auto path_resolved = resolve_path(path);
+
+		if (shader)
+		{
+			force_reload = true;
+			cache_result = false;
+		}
 
 		if (!force_reload)
 		{
@@ -63,15 +73,18 @@ namespace engine
 				return { std::move(loaded_model), nullptr };
 			}
 		}
-		
-		auto model_data = graphics::Model::Load(get_context(), path, get_default_shader(), load_collision);
+
+		auto model_data = graphics::Model::Load(get_context(), path, ((shader) ? shader : get_default_shader()), load_collision);
 
 		auto loaded_model = memory::allocate<graphics::Model>(); // ModelRef
 		*loaded_model = std::move(std::get<0>(model_data));
 
 		ASSERT(loaded_model->has_meshes());
 
-		loaded_models[path_resolved] = loaded_model;
+		if (cache_result)
+		{
+			loaded_models[path_resolved] = loaded_model;
+		}
 
 		if (load_collision)
 		{
@@ -86,6 +99,34 @@ namespace engine
 		}
 
 		return { std::move(loaded_model), nullptr };
+	}
+
+	ResourceManager::ShaderRef ResourceManager::get_shader(const std::string& vertex_path, const std::string& fragment_path, bool force_reload, bool cache_result) const
+	{
+		auto vert_resolved = resolve_path(vertex_path);
+		auto frag_resolved = resolve_path(fragment_path);
+
+		auto shader_uid = util::concat(vertex_path, "|", fragment_path);
+
+		if (!force_reload)
+		{
+			// TODO: Implement optimization for underlying shader-objects. (e.g. re-use vertex shader at link-time)
+			auto lookup = util::get_if_exists<ShaderRef>(loaded_shaders, shader_uid);
+
+			if (lookup)
+			{
+				return *lookup;
+			}
+		}
+
+		auto shader = std::make_shared<graphics::Shader>(context, vertex_path, fragment_path);
+
+		if (cache_result)
+		{
+			loaded_shaders[shader_uid] = shader;
+		}
+
+		return shader;
 	}
 
 	ResourceManager::CollisionData ResourceManager::get_capsule_collision(float radius, float height) // ref<btCapsuleShape>
