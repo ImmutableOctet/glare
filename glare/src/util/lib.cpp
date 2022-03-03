8f39ed086ef4f449eff3d027046d63664f7e2d17
Anthony Diamond <sonickid8@live.com>
2022-03-02 17:29:21 -0700

    Added OpenGL version specification.

--- glare/src/util/lib.cpp
+++ glare/src/util/lib.cpp
@@ -20,23 +20,21 @@ namespace util
 			return true;
 		}
 
-		bool establish_gl()
+		OpenGLVersion establish_gl(std::optional<OpenGLVersion> version)
 		{
+			if (!version.has_value())
+				version = { 4, 3 };
+
 			// Establish Core Profile Context:
-			/*
-			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, version->major);
+			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, version->minor);
 			SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-			*/
 
-			///*
-			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-			SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-			SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
-			//*/
+			#ifdef NDEBUG
+				SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
+			#endif
 
-			return true;
+			return *version;
 		}
 	}
 }
--- glare/src/util/lib.hpp
+++ glare/src/util/lib.hpp
@@ -1,12 +1,23 @@
 #pragma once
 
 #include <debug.hpp>
+#include <types.hpp>
+
+#include <optional>
 
 namespace util
 {
 	namespace lib
 	{
+		struct OpenGLVersion
+		{
+			using version_t = std::uint8_t;
+
+			version_t major;
+			version_t minor;
+		};
+
 		bool init_sdl();
-		bool establish_gl();
+		OpenGLVersion establish_gl(std::optional<OpenGLVersion> version=std::nullopt);
 	}
 }
