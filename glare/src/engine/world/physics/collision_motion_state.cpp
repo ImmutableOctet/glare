6068f8317fad7bee9b14802d987ad406797b31de
Anthony Diamond <sonickid8@live.com>
2022-08-31 02:53:38 -0600

    Added logic for exact collision-groups when calling `make_collision_motion_state`.

--- glare/src/engine/world/physics/collision_motion_state.cpp
+++ glare/src/engine/world/physics/collision_motion_state.cpp
@@ -30,11 +30,20 @@ namespace engine
 
 	std::unique_ptr<CollisionMotionState> make_collision_motion_state(World& world, Entity entity, const CollisionConfig& config)
 	{
-		if (config.solid_mask != CollisionGroup::None)
+		if (config.solid_mask == CollisionGroup::None)
 		{
-			return std::make_unique<CollisionMotionState>(world, entity);
+			return {};
 		}
 
-		return {};
+		// Exact group values:
+		switch (config.group)
+		{
+			case CollisionGroup::StaticGeometry:
+			case CollisionGroup::Zone:
+			//case CollisionGroup::Particle:
+				return {};
+		}
+
+		return std::make_unique<CollisionMotionState>(world, entity);
 	}
 }
