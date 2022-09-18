e9ece21c0c023562cca0659ae551a46e5b84e9e8
Anthony Diamond <sonickid8@live.com>
2022-09-18 08:39:22 -0600

    Implemented AABB type in `math`.

---
+++ glare/src/math/aabb.cpp
@@ -0,0 +1,31 @@
+#include "aabb.hpp"
+
+#include <glm/glm.hpp>
+
+namespace math
+{
+	Vector AABB::dim_lengths() const
+	{
+		return (max - min);
+	}
+
+	float AABB::length() const
+	{
+		return glm::length(dim_lengths());
+	}
+
+	float AABB::average_length() const
+	{
+		auto len = dim_lengths();
+
+		return ((len.x + len.y + len.z) / 3.0f);
+	}
+
+	Vector AABB::get_center_point() const
+	{
+		//auto half_len = (dim_lengths() * 0.5f);
+		//return { (min.x + half_len.x), (min.y + half_len.y), (min.z + half_len.z) };
+
+		return { ((min.x + max.x) * 0.5f), ((min.y + max.y) * 0.5f), ((min.z + max.z) * 0.5f) };
+	}
+}
