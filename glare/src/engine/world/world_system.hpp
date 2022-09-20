270c31045285f071d88a7b44c74d097c6a3c7473
Anthony Diamond <sonickid8@live.com>
2022-09-20 03:00:37 -0600

    Reworked `WorldSystem` to handle `world` reference internally, but still allow for multiple services if enabled.

--- glare/src/engine/world/world_system.hpp
+++ glare/src/engine/world/world_system.hpp
@@ -13,6 +13,8 @@ namespace engine
 {
 	// Forward declaractions:
 	class World;
+	class SystemManager;
+	class Service;
 
 	// Event types:
 	struct OnServiceUpdate;
@@ -22,14 +24,70 @@ namespace engine
 	class WorldSystem
 	{
 		public:
+			friend class SystemManager;
+
+			// NOTE: Subscription is deferred until the system is fully constructed.
+			// See also: `SystemManager`.
+			WorldSystem(World& world, bool allow_multiple_subscriptions=false);
 			virtual ~WorldSystem();
 
-			void subscribe(World& world);
-			void unsubscribe(World& world);
+			//WorldSystem(WorldSystem&&) = delete;
+			//WorldSystem(const WorldSystem&) = delete;
+
+			/*
+				NOTES:
+				
+				It is safe to call this method multiple times for the internal `world` object.
+				
+				If `allow_multiple_subscriptions` is true, it is valid to call this method with a different
+				`World` object than `WorldSystem::world`, as long as no more than one concurrent subscription is active.
+
+				Multiple concurrent subscriptions to the same service are considered undefined behavior.
+			*/
+			bool subscribe(World& world);
+
+			// See notes for `subscribe`.
+			bool unsubscribe(World& world);
+
+			// Retrieves the internal `World` object this service was created with.
+			// NOTE: This `World` object may only be 'symbolically linked' if a subscription has not taken place.
+			// To enasure that subscription has happened successfully, please check against `is_subscribed`.
+			inline World& get_world() const { return world; }
+
+			// Indicates whether a subscription has been established with the internal `World` object.
+			// This does not report when other services have subscriptions with this system.
+			inline bool is_subscribed() const { return subscribed; }
+
+			// Returns true if `svc` points to the same object as `WorldSystem::world`.
+			bool is_bound_world(Service* svc) const;
+
+			// Returns true if `world` is the same as `WorldSystem::world`.
+			bool is_bound_world(World* world) const;
+
+			// Returns true if `svc` is the same as `WorldSystem::world`.
+			inline bool is_bound_world(Service& svc) const { return is_bound_world(&svc); }
+
+			// Returns true if `world` points to the same object as `WorldSystem::world`.
+			inline bool is_bound_world(World& world) const { return is_bound_world(&world); }
 		private:
+			// Used internally. (Declared here, defined in source file)
+			template <typename ServiceEventType>
+			World* resolve_world(const ServiceEventType& event_obj);
+
 			void update(const OnServiceUpdate& update_event);
 			void render(const OnServiceRender& render_event);
 		protected:
+			// Safely retrieves a `World` pointer from a base `Service` pointer.
+			// NOTE: If `allow_multiple_subscriptions` is false, this will only
+			// return non-null when `WorldSystem::world` is supplied.
+			World* resolve_world(Service* service);
+
+			// Subscribes to the internally stored `world` object.
+			inline bool subscribe() { return subscribe(world); }
+
+			// Unsubscribes from the internally stored `world` object.
+			inline bool unsubscribe() { return unsubscribe(world); }
+
 			// Called by `subscribe` after default subscription actions are performed.
 			virtual void on_subscribe(World& world) = 0;
 			virtual void on_update(World& world, float delta) = 0;
@@ -39,5 +97,16 @@ namespace engine
 
 			// Empty implementation provided by default.
 			virtual void on_unsubscribe(World& world);
+
+			// The `World` instance this system is linked to.
+			World& world;
+
+			// Flags:
+
+			// Whether we're subscribed to `world`.
+			bool subscribed                   : 1;
+
+			// Whether we allow for multiple subscriptions concurrently.
+			bool allow_multiple_subscriptions : 1;
 	};
 }
