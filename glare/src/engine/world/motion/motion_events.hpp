9f87990556f7144fc0814bc881d0593ecb936ada
Anthony Diamond <sonickid8@live.com>
2022-09-21 01:34:53 -0600

    Added comments for motion-event types.

--- glare/src/engine/world/motion/motion_events.hpp
+++ glare/src/engine/world/motion/motion_events.hpp
@@ -12,23 +12,31 @@ namespace engine
 	struct OnAirToGround
 	{
 		//Entity entity;
+
+		// The surface (ground) we contacted.
 		Ground surface;
 
+		// A length-vector representing the movement
+		// that caused contact with the ground.
 		inline math::Vector landing_vector() const
 		{
 			return surface.impact_velocity;
 		}
 
+		// The entity that collided with the ground.
+		// NOTE: Opposite of `surface`'s definition.
 		inline Entity entity() const
 		{
 			return surface.collision.a;
 		}
 
+		// The ground entity we're contacting.
 		inline Entity ground() const
 		{
 			return surface.entity();
 		}
 
+		// The world-space position of the ground we contacted.
 		inline math::Vector ground_position() const
 		{
 			return surface.collision.position;
@@ -40,16 +48,21 @@ namespace engine
 	struct OnGroundToAir
 	{
 		//Entity entity;
+
+		// The surface (ground) we're leaving from.
 		Ground surface;
 
 		math::Vector entity_position;
 		math::Vector escape_vector;
 
+		// The entity that is leaving the ground.
+		// NOTE: Opposite of `surface`'s definition.
 		inline Entity entity() const
 		{
 			return surface.contacting_entity();
 		}
 
+		// The ground entity we're leaving.
 		inline Entity ground() const
 		{
 			return surface.entity();
