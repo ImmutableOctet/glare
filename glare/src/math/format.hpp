fd66c3e70cb32ff44dc03e5d03b2d03e6e4f99c6
Anthony Diamond <sonickid8@live.com>
2022-09-27 04:44:55 -0600

    Migrated formatter implementations from `util:log` to `format` files in each module.

---
+++ glare/src/math/format.hpp
@@ -0,0 +1,100 @@
+#pragma once
+
+#include "types.hpp"
+
+#include <util/format.hpp>
+
+template <>
+struct fmt::formatter<math::Vector2D>
+{
+    // Presentation format: 'f' - fixed, 'e' - exponential.
+    char presentation = 'f';
+
+    // Parses format specifications of the form ['f' | 'e'].
+    constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin())
+    {
+        // Parse the presentation format and store it in the formatter:
+        auto it = ctx.begin(), end = ctx.end();
+
+        if (it != end && (*it == 'f' || *it == 'e'))
+            presentation = *it++;
+
+        // Check if reached the end of the range.
+        if (it != end && *it != '}')
+            throw format_error("invalid format");
+
+        // Return an iterator past the end of the parsed range.
+        return it;
+    }
+
+    template <typename FormatContext>
+    auto format(const math::Vector2D& v, FormatContext& ctx) -> decltype(ctx.out())
+    {
+        return (presentation == 'f')
+            ? fmt::format_to(ctx.out(), "({:.1f}, {:.1f})", v.x, v.y)
+            : fmt::format_to(ctx.out(), "({:.1e}, {:.1e})", v.x, v.y);
+    }
+};
+
+template <>
+struct fmt::formatter<math::Vector3D>
+{
+    // Presentation format: 'f' - fixed, 'e' - exponential.
+    char presentation = 'f';
+
+    // Parses format specifications of the form ['f' | 'e'].
+    constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin())
+    {
+        // Parse the presentation format and store it in the formatter:
+        auto it = ctx.begin(), end = ctx.end();
+
+        if (it != end && (*it == 'f' || *it == 'e'))
+            presentation = *it++;
+
+        // Check if reached the end of the range.
+        if (it != end && *it != '}')
+            throw format_error("invalid format");
+
+        // Return an iterator past the end of the parsed range.
+        return it;
+    }
+
+    template <typename FormatContext>
+    auto format(const math::Vector3D& v, FormatContext& ctx) -> decltype(ctx.out())
+    {
+        return (presentation == 'f')
+            ? fmt::format_to(ctx.out(), "({:.1f}, {:.1f}, {:.1f})", v.x, v.y, v.z)
+            : fmt::format_to(ctx.out(), "({:.1e}, {:.1e}, {:.1e})", v.x, v.y, v.z);
+    }
+};
+
+template <>
+struct fmt::formatter<math::TransformVectors>
+{
+    constexpr auto parse(format_parse_context& ctx)
+    {
+        auto it = ctx.begin(), end = ctx.end();
+
+        // Check if reached the end of the range:
+        if (it != end && *it != '}')
+            throw format_error("invalid format");
+
+        // Return an iterator past the end of the parsed range:
+        return it;
+    }
+
+    template <typename FormatContext>
+    auto format(const math::TransformVectors& v, FormatContext& ctx)
+    {
+        const auto& [position, rotation, scale] = v;
+
+        return format_to
+        (
+            ctx.out(),
+            "[({:.1f}, {:.1f}, {:.1f}), ({:.1f}, {:.1f}, {:.1f}), ({:.1f}, {:.1f}, {:.1f})]",
+            position.x, position.y, position.z,
+            rotation.x, rotation.y, rotation.z,
+            scale.x, scale.y, scale.z
+        );
+    }
+};
