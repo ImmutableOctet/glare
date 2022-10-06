445dc9214eeebdfd900da164d31a71a59494b98e
Anthony Diamond <sonickid8@live.com>
2022-10-06 03:07:39 -0600

    Expanded `MouseState` to cover forward and back, as well as mouse wheel motion.

--- glare/src/app/input/mouse_state.hpp
+++ glare/src/app/input/mouse_state.hpp
@@ -7,11 +7,23 @@ namespace app::input
 {
 	struct MouseState
 	{
+		// Relative motion on the X-axis.
 		int x = 0;
+
+		// Relative motion on the Y-axis.
 		int y = 0;
 
-		bool left   : 1 = false;
-		bool middle : 1 = false;
-		bool right  : 1 = false;
+		// Relative wheel motion on the X-axis.
+		int wheel_x = 0;
+
+		// Relative wheel motion on the Y-axis.
+		int wheel_y = 0;
+
+		// Button states:
+		bool left    : 1 = false;
+		bool middle  : 1 = false;
+		bool right   : 1 = false;
+		bool back    : 1 = false;
+		bool forward : 1 = false;
 	};
 }
