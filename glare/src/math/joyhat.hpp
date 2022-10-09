b6d5d34c46852af09d739d554792d66c96282799
Anthony Diamond <sonickid8@live.com>
2022-10-09 01:59:50 -0600

    Updated `math::joyhat` logic to account for opposite directions being pressed, moved the implementation into a separate source file, etc.

--- glare/src/math/joyhat.hpp
+++ glare/src/math/joyhat.hpp
@@ -5,33 +5,6 @@
 
 namespace math
 {
-	inline Vector2D joyhat(bool up, bool down, bool left, bool right)
-	{
-		Vector2D direction = {};
-
-		if (up)
-		{
-			direction.y = 1.0f;
-		}
-		else if (down)
-		{
-			direction.y = -1.0f;
-		}
-
-		if (left)
-		{
-			direction.x = -1.0f;
-		}
-		else if (right)
-		{
-			direction.x = 1.0f;
-		}
-
-		return direction;
-	}
-
-	inline float joyhat_angle(bool up, bool down, bool left, bool right)
-	{
-		return direction_to_angle(joyhat(up, down, left, right));
-	}
+	Vector2D joyhat(bool up, bool down, bool left, bool right);
+	float joyhat_angle(bool up, bool down, bool left, bool right);
 }
