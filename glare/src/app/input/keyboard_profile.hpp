c3bf95d160267c12e029ce887020178011a70a9b
Anthony Diamond <sonickid8@live.com>
2022-10-10 05:18:42 -0600

    Implemented `KeyboardHat` functionality in `KeyboardProfile`, etc.

--- glare/src/app/input/keyboard_profile.hpp
+++ glare/src/app/input/keyboard_profile.hpp
@@ -6,10 +6,18 @@
 // Keyboard button/scan codes in the form of an enum.
 #include "keyboard_buttons.hpp"
 
+// Keyboard Hat switch simulation.
+#include "keyboard_hat.hpp"
+
 // Placeholder type.
 #include "keyboard_motion.hpp"
 
 #include <util/json.hpp>
+
+#include <string>
+#include <optional>
+#include <vector>
+//#include <unordered_map>
 
 namespace app::input
 {
@@ -17,6 +25,9 @@ namespace app::input
 
 	struct KeyboardProfile : public InputProfile<KeyboardButton, KeyboardMotion>
 	{
+		using Hat = KeyboardHat;
+		using HatContainer = std::vector<Hat>;
+
 		KeyboardProfile() = default;
 		KeyboardProfile(const KeyboardProfile&) = default;
 		KeyboardProfile(KeyboardProfile&&) noexcept = default;
@@ -27,5 +38,14 @@ namespace app::input
 		KeyboardProfile(const ProfileMetadata& profile_metadata, const util::json& json);
 
 		void load(const ProfileMetadata& profile_metadata, const util::json& json);
+		void load_hats(const ProfileMetadata& profile_metadata, const util::json& json);
+
+		std::optional<std::size_t> get_hat_index(std::string_view hat_name) const;
+		const Hat* get_hat(std::string_view hat_name) const;
+
+		const HatContainer& get_hats() const { return hat_descriptors; }
+		HatContainer& get_hats() { return hat_descriptors; }
+
+		HatContainer hat_descriptors; // std::unordered_map<std::string, Hat>
 	};
 }
