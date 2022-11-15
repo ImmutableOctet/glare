037478805b61981e5daa06596a82ef72a36f8b79
Anthony Diamond <sonickid8@live.com>
2022-11-15 16:09:00 -0700

    Updated source references in `engine`.

--- glare/src/engine/engine.hpp
+++ glare/src/engine/engine.hpp
@@ -2,8 +2,19 @@
 
 #include "types.hpp"
 #include "config.hpp"
+
+//#include "components/components.hpp"
+
+//#include "forwarding_component.hpp"
+//#include "model_component.hpp"
+
+#include "components/name_component.hpp"
+#include "components/type_component.hpp"
+#include "components/relationship_component.hpp"
+#include "components/transform_component.hpp"
+
 #include "transform.hpp"
-#include "relationship.hpp"
 
 #include "resource_manager/resource_manager.hpp"
+
 #include "world/world.hpp"
