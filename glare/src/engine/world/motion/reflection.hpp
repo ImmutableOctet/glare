f6508f4f2c72b7210d3e717f7a62cdbdb811b442
Anthony Diamond <sonickid8@live.com>
2022-11-17 04:30:39 -0700

    Added motion event types to reflection.

--- glare/src/engine/world/motion/reflection.hpp
+++ glare/src/engine/world/motion/reflection.hpp
@@ -10,6 +10,8 @@
 #include "components/velocity_component.hpp"
 #include "components/deceleration_component.hpp"
 
+#include "motion_events.hpp"
+
 namespace engine
 {
 	class MotionSystem;
@@ -50,12 +52,63 @@ namespace engine
 	template <>
 	void reflect<MotionComponent>()
 	{
+		// TODO: Add getter-methods for boolean data members.
 		engine_meta_type<MotionComponent>();
+	}
+
+	template <>
+	void reflect<OnAirToGround>()
+	{
+		engine_meta_type<OnAirToGround>()
+			.data<&OnAirToGround::surface>("surface"_hs)
+			.data<nullptr, &OnAirToGround::landing_vector>("landing_vector"_hs)
+			.data<nullptr, &OnAirToGround::ground>("ground"_hs)
+			.data<nullptr, &OnAirToGround::ground_position>("ground_position"_hs)
+			.data<nullptr, &OnAirToGround::is_dynamic_ground>("is_dynamic_ground"_hs)
+			.ctor<decltype(OnAirToGround::surface)>()
+		;
+	}
+
+	template <>
+	void reflect<OnGroundToAir>()
+	{
+		engine_meta_type<OnGroundToAir>()
+			.data<&OnGroundToAir::surface>("surface"_hs)
+			.data<&OnGroundToAir::entity_position>("entity_position"_hs)
+			.data<&OnGroundToAir::escape_vector>("escape_vector"_hs)
+			.data<nullptr, &OnGroundToAir::ground>("ground"_hs)
+			.data<nullptr, &OnGroundToAir::was_dynamic_ground>("was_dynamic_ground"_hs)
+			.ctor
+			<
+				decltype(OnGroundToAir::surface),
+				decltype(OnGroundToAir::entity_position),
+				decltype(OnGroundToAir::escape_vector)
+			>()
+		;
+	}
+
+	template <>
+	void reflect<OnMotionAttachment>()
+	{
+		engine_meta_type<OnMotionAttachment>()
+			.data<&OnMotionAttachment::attached_to>("attached_to"_hs)
+			.ctor<decltype(OnMotionAttachment::entity), decltype(OnMotionAttachment::attached_to)>()
+		;
+	}
+
+	template <>
+	void reflect<OnMotionDetachment>()
+	{
+		engine_meta_type<OnMotionDetachment>()
+			.data<&OnMotionDetachment::detached_from>("detached_from"_hs)
+			.ctor<decltype(OnMotionDetachment::entity), decltype(OnMotionDetachment::detached_from)>()
+		;
 	}
 
 	template <>
 	void reflect<MotionSystem>()
 	{
+		// Components:
 		reflect<MotionComponent>();
 		reflect<AlignmentComponent>();
 		reflect<VelocityComponent>();
@@ -63,5 +116,11 @@ namespace engine
 		reflect<DecelerationComponent>();
 		reflect<GroundComponent>();
 		reflect<MotionAttachmentProxyComponent>();
+
+		// Events:
+		reflect<OnAirToGround>();
+		reflect<OnGroundToAir>();
+		reflect<OnMotionAttachment>();
+		reflect<OnMotionDetachment>();
 	}
 }
