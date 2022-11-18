6546987d27ba23f58d61ba884e3c5c3d30c159c1
Anthony Diamond <sonickid8@live.com>
2022-11-18 04:30:33 -0700

    Added `service`-related traits.

--- glare/src/engine/meta/traits.hpp
+++ glare/src/engine/meta/traits.hpp
@@ -14,6 +14,10 @@ namespace engine
 	GENERATE_HAS_METHOD_TRAIT(get_entity); // `has_method_get_entity`
 	GENERATE_HAS_FIELD_TRAIT(entity); // `has_field_entity`
 
+	GENERATE_HAS_METHOD_TRAIT(service); // `has_method_service`
+	GENERATE_HAS_METHOD_TRAIT(get_service); // `has_method_get_service`
+	GENERATE_HAS_FIELD_TRAIT(service); // `has_field_service`
+
 	// Allows for types to (optionally) define their own `reflect` static member-functions.
 	// See also: `engine::reflect` free-function. (found in `reflection`)
 	GENERATE_HAS_FUNCTION_TRAIT(reflect); // `has_function_reflect`
