93c665e17df199ad4e1933b4cf680abfb192fbc9
Anthony Diamond <sonickid8@live.com>
2022-11-18 04:32:46 -0700

    Implemented various reflection for event-types in `engine:input`, switched to dedicated translation unit, etc.

--- glare/src/engine/input/reflection.hpp
+++ glare/src/engine/input/reflection.hpp
@@ -2,28 +2,12 @@
 
 #include <engine/reflection.hpp>
 
-#include "components/input_component.hpp"
+//#include "reflection_impl_input.cpp"
 
 namespace engine
 {
 	class InputSystem;
 
-	template <>
-	void reflect<InputComponent>()
-	{
-		//REFLECT_SINGLE_FIELD_COMPONENT(InputComponent, input_index);
-
-		engine_meta_type<InputComponent>()
-			.data<nullptr, &InputComponent::get_input_index>("input_index"_hs)
-			.ctor<decltype(InputComponent::input_index)>()
-		;
-	}
-
-	template <>
-	void reflect<InputSystem>()
-	{
-		reflect<InputComponent>();
-
-		// ...
-	}
+	 template <>
+	 extern void reflect<InputSystem>();
 }
