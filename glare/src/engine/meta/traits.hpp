ec05445046e6d28807263de2e36de449a91cd790
Anthony Diamond <sonickid8@live.com>
2022-11-28 16:02:05 -0700

    Added traits for `player` (`Entity`) and `player_index` (`PlayerIndex`).

--- glare/src/engine/meta/traits.hpp
+++ glare/src/engine/meta/traits.hpp
@@ -18,6 +18,14 @@ namespace engine
 	GENERATE_HAS_METHOD_TRAIT(get_service); // `has_method_get_service`
 	GENERATE_HAS_FIELD_TRAIT(service); // `has_field_service`
 
+	GENERATE_HAS_METHOD_TRAIT(player); // `has_method_player`
+	GENERATE_HAS_METHOD_TRAIT(get_player); // `has_method_get_player`
+	GENERATE_HAS_FIELD_TRAIT(player); // `has_field_player`
+
+	GENERATE_HAS_METHOD_TRAIT(player_index); // `has_method_player_index`
+	GENERATE_HAS_METHOD_TRAIT(get_player_index); // `has_method_get_player_index`
+	GENERATE_HAS_FIELD_TRAIT(player_index); // `has_field_player_index`
+
 	// Allows for types to (optionally) define their own `reflect` static member-functions.
 	// See also: `engine::reflect` free-function. (found in `reflection`)
 	GENERATE_HAS_FUNCTION_TRAIT(reflect); // `has_function_reflect`
