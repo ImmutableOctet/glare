d2cc4f2a7f6dd7df28934ea7d6b71ae6cb4faa68
Anthony Diamond <sonickid8@live.com>
2022-11-28 16:10:15 -0700

    Added initial (WIP) 'rule' support to `StateSystem`.

--- glare/src/engine/state/state_system.cpp
+++ glare/src/engine/state/state_system.cpp
@@ -13,6 +13,9 @@
 
 #include <engine/resource_manager/resource_manager.hpp>
 
+// Debugging related:
+#include <util/log.hpp>
+
 namespace engine
 {
 	StateSystem::StateSystem(Service& service, const ResourceManager& resource_manager, bool subscribe_immediately)
@@ -26,10 +29,26 @@ namespace engine
 
 	bool StateSystem::on_subscribe(Service& service)
 	{
-		//auto& registry = service.get_registry();
+		auto& registry = service.get_registry();
 
-		// Meta:
+		registry.on_construct<StateComponent>().connect<&StateSystem::on_state_init>(*this);
+		registry.on_update<StateComponent>().connect<&StateSystem::on_state_update>(*this);
+		registry.on_destroy<StateComponent>().connect<&StateSystem::on_state_destroyed>(*this);
+
 		service.register_event<OnServiceUpdate, &StateSystem::on_update>(*this);
+
+		return true;
+	}
+
+	bool StateSystem::on_unsubscribe(Service& service)
+	{
+		auto& registry = service.get_registry();
+
+		registry.on_construct<StateComponent>().disconnect(*this);
+		registry.on_update<StateComponent>().disconnect(*this);
+		registry.on_destroy<StateComponent>().disconnect(*this);
+
+		service.unregister(*this);
 
 		return true;
 	}
@@ -43,20 +62,14 @@ namespace engine
 	{
 		auto& registry = get_registry();
 
-		auto* instance_details = registry.try_get<InstanceComponent>(entity);
+		const auto* instance_details = registry.try_get<InstanceComponent>(entity);
 
 		if (!instance_details)
 		{
 			return nullptr;
 		}
-		
-		const auto* factory_data = resource_manager.get_existing_factory(instance_details->instance);
 
-		assert(factory_data);
-
-		const auto& descriptor = factory_data->factory.get_descriptor();
-
-		return &descriptor;
+		return resource_manager.get_existing_descriptor(instance_details->instance);
 	}
 
 	std::optional<EntityStateIndex> StateSystem::get_state(Entity entity) const
@@ -91,7 +104,7 @@ namespace engine
 		if (descriptor->set_state_by_id(registry, entity, current_state, state_id))
 		{
 			const auto& from_state_index = *current_state;
-			const auto& from_state_id = descriptor->states[from_state_index].name;
+			const auto& from_state_id = descriptor->states[from_state_index]->name;
 
 			const auto new_state = get_state(entity);
 
@@ -130,10 +143,10 @@ namespace engine
 		if (descriptor->set_state_by_index(registry, entity, current_state, state_index))
 		{
 			const auto& from_state_index = *current_state;
-			const auto& from_state_id = descriptor->states[from_state_index].name;
+			const auto& from_state_id = descriptor->states[from_state_index]->name;
 
 			const auto& to_state_index = state_index;
-			const auto& to_state_id = descriptor->states[to_state_index].name;
+			const auto& to_state_id = descriptor->states[to_state_index]->name;
 
 			service->queue_event<OnStateChange>
 			(
@@ -152,5 +165,92 @@ namespace engine
 	void StateSystem::on_update(const OnServiceUpdate& data)
 	{
 		// ...
+	}
+
+	void StateSystem::on_state_init(Registry& registry, Entity entity)
+	{
+		on_state_update_impl(registry, entity, false);
+	}
+
+	void StateSystem::on_state_update(Registry& registry, Entity entity)
+	{
+		on_state_update_impl(registry, entity);
+	}
+
+	void StateSystem::on_state_update_impl(Registry& registry, Entity entity, bool handle_existing)
+	{
+		const auto* descriptor = get_descriptor(entity);
+
+		assert(descriptor);
+
+		const auto& state_component = registry.get<StateComponent>(entity);
+
+		if (handle_existing)
+		{
+			// No state-change occurred, no need to update anything.
+			if (state_component.state_index == state_component.prev_state_index)
+			{
+				return;
+			}
+
+			for (auto& listener_entry : rule_listeners)
+			{
+				auto& listener = listener_entry.second;
+
+				const auto prev_state_index = state_component.prev_state_index;
+				const auto& prev_state = *descriptor->states[prev_state_index];
+
+				listener.remove_rules(prev_state);
+			}
+		}
+
+		const auto state_index = state_component.state_index;
+
+		const auto& state = *descriptor->states[state_index];
+
+		for (const auto& rule_entries : state.rules)
+		{
+			const auto& event_type_id = rule_entries.first;
+			const auto& state_rules   = rule_entries.second;
+
+			auto& listener = rule_listeners[event_type_id];
+
+			if (!listener.is_active())
+			{
+				auto event_type = resolve(event_type_id);
+
+				if (!event_type)
+				{
+					print_warn("Unable to resolve meta-type for event: #{}", event_type_id);
+
+					continue;
+				}
+
+				auto connect_fn = event_type.func(hash("connect_meta_event"));
+
+				if (!connect_fn)
+				{
+					print_warn("Unable to resolve connection function for meta event-listener -- type: #{}", event_type_id);
+
+					continue;
+				}
+
+				auto& listener_ref = reinterpret_cast<MetaEventListener&>(listener);
+
+				auto result = connect_fn.invoke({}, entt::forward_as_meta(listener_ref), entt::forward_as_meta(this->service));
+
+				if (!result)
+				{
+					print_warn("Failed to invoke meta event-listener connection routine. -- type: #{}", event_type_id);
+				}
+			}
+
+			listener.add_rules(state); // , event_type_id
+		}
+	}
+
+	void StateSystem::on_state_destroyed(Registry& registry, Entity entity)
+	{
+
 	}
 }
--- glare/src/engine/state/state_system.hpp
+++ glare/src/engine/state/state_system.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
+#include "entity_rule_listener.hpp"
+
 #include <engine/types.hpp>
 #include <engine/basic_system.hpp>
 #include <engine/meta/types.hpp>
 
+#include <util/small_vector.hpp>
+
 #include <string_view>
 #include <optional>
+
+#include <unordered_map>
 
 namespace engine
 {
@@ -23,6 +29,9 @@ namespace engine
 			bool set_state_by_id(Entity entity, StringHash state_id) const;
 			bool set_state_by_index(Entity entity, EntityStateIndex state_index) const;
 		protected:
+
+			std::unordered_map<MetaTypeID, EntityRuleListener> rule_listeners;
+
 			Registry& get_registry() const;
 
 			const EntityDescriptor* get_descriptor(Entity entity) const;
@@ -30,10 +39,16 @@ namespace engine
 
 			// Triggered when this system subscribes to a service.
 			bool on_subscribe(Service& service) override;
+			bool on_unsubscribe(Service& service) override;
 
 			// Triggered once per service update; used to handle
 			// things like 'state changed' (`OnInput`) events, etc.
 			void on_update(const OnServiceUpdate& data);
+
+			void on_state_init(Registry& registry, Entity entity);
+			void on_state_update(Registry& registry, Entity entity);
+			void on_state_update_impl(Registry& registry, Entity entity, bool handle_existing=true);
+			void on_state_destroyed(Registry& registry, Entity entity);
 
 			const ResourceManager& resource_manager;
 	};
