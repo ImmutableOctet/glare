8f6f4b838a401ba07c7ba567da4f2b160ee383df
Anthony Diamond <sonickid8@live.com>
2022-12-01 01:29:44 -0700

    Added `OnStateActivate` type, added `state_activated` flag to `OnStateChange`.

--- glare/src/engine/state/events.hpp
+++ glare/src/engine/state/events.hpp
@@ -4,11 +4,27 @@
 
 namespace engine
 {
+	// Triggers when a state change takes place.
 	struct OnStateChange
 	{
 		Entity entity;
 
 		EntityStateInfo from;
 		EntityStateInfo to;
+
+		// Indicates whether the state described by `to` has already been activated.
+		// 
+		// For most state-change scenarios, this value will be true, meaning the initial changes
+		// imposed by switching to the new state have been applied. -- However, a state can utilize
+		// a self-imposed delay in activation, which will delay changes for a certain amount of time.
+		bool state_activated = true;
+	};
+
+	// Triggers after a state has finished activating.
+	struct OnStateActivate
+	{
+		Entity entity;
+
+		EntityStateInfo state;
 	};
 }
