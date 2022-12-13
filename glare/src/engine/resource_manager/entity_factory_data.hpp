1ac6552851c4cf7f4088eaeb7a3c32b539e872ef
Anthony Diamond <sonickid8@live.com>
2022-12-12 17:09:58 -0700

    Added to the `EntityFactoryData` interface to better allow for manual instantiation of child factories, etc.

--- glare/src/engine/resource_manager/entity_factory_data.hpp
+++ glare/src/engine/resource_manager/entity_factory_data.hpp
@@ -9,15 +9,29 @@
 namespace engine
 {
 	using EntityFactoryKey = EntityFactory::FactoryKey; // std::string; // std::filsystem::path;
-	using EntityFactoryChildren = util::small_vector<EntityFactoryKey, 2>; // 4 // 8
+	using EntityFactoryChildren = util::small_vector<EntityFactoryKey, 8>; // 16
 
 	struct EntityFactoryData
 	{
 		using FactoryKey = EntityFactoryKey;
 
+		// The underlying factory instance.
 		EntityFactory factory;
+
+		// List of factory instance paths, representing children of entities
+		// produced by `create` and related functions.
 		EntityFactoryChildren children;
 
-		Entity create(const EntityConstructionContext& context) const;
+		// Generates an entity using `context`.
+		Entity create(const EntityConstructionContext& context, bool handle_children=true) const;
+
+		// Generates the child entities associated with this factory, then adds them as children to `entity`.
+		// Entities are generated from referenced factories (via instance-path) -- i.e. `EntityFactoryData` instances.
+		// 
+		// NOTE: If `parent` is `null` or omitted, this will attempt to use the `parent` field of `context`.
+		bool generate_children(const EntityConstructionContext& child_context, Entity parent) const;
+
+		// Generates the child entities associated with this factory, then adds them as children to `context.parent`.
+		bool generate_children(const EntityConstructionContext& child_context) const;
 	};
 }
