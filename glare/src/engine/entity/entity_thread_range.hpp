ba2c0a009ee2e5a83c2c7ec28bfbfa52772829c4
Anthony Diamond <sonickid8@live.com>
2023-01-17 05:41:22 -0700

    Initial implementation of `EntityThreadRange`.

---
+++ glare/src/engine/entity/entity_thread_range.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "types.hpp"
+
+namespace engine
+{
+	struct EntityThreadRange
+	{
+		EntityThreadIndex start_index;
+		EntityThreadCount thread_count;
+
+		inline EntityThreadIndex begin_point() const
+		{
+			return start_index;
+		}
+
+		inline EntityThreadIndex end_point() const
+		{
+			return (start_index + thread_count);
+		}
+
+		inline std::size_t size() const
+		{
+			return static_cast<std::size_t>(thread_count);
+		}
+
+		inline bool empty() const
+		{
+			return (size() == 0);
+		}
+	};
+}
