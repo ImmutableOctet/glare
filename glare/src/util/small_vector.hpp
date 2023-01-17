451bfe5aa1e8162eb2e537d8c98dd17b96d8ebb4
Anthony Diamond <sonickid8@live.com>
2023-01-17 03:23:14 -0700

    Added comments related to `_SILENCE_CXX23_ALIGNED_STORAGE_DEPRECATION_WARNING`.

--- glare/src/util/small_vector.hpp
+++ glare/src/util/small_vector.hpp
@@ -1,6 +1,12 @@
 #pragma once
 
+// Needed on latest MSVC versions when including Folly's `small_vector` type,
+// since it uses `std::aligned_storage` and related deprecated functionality.
+//#define _SILENCE_CXX23_ALIGNED_STORAGE_DEPRECATION_WARNING 1
+
 #include <folly/small_vector.h>
+
+//#undef _SILENCE_CXX23_ALIGNED_STORAGE_DEPRECATION_WARNING
 
 namespace util
 {
