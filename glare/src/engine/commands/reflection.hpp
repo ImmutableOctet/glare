7fb973d47aa12a795e6840b985b7cf3a2ceb77e5
Anthony Diamond <sonickid8@live.com>
2023-01-17 02:52:51 -0700

    Changed usage of `engine_meta_type` to `engine_command_type` for reflection of certain command-types, etc.

--- glare/src/engine/commands/reflection.hpp
+++ glare/src/engine/commands/reflection.hpp
@@ -2,6 +2,9 @@
 
 #include <engine/reflection.hpp>
 
+// TODO: Move this file into the `commands` submodule.
+#include <engine/command.hpp>
+
 #include "print_command.hpp"
 #include "component_patch_command.hpp"
 #include "component_replace_command.hpp"
@@ -9,10 +12,20 @@
 namespace engine
 {
 	template <>
+    void reflect<Command>()
+    {
+        engine_meta_type<Command>(false)
+			//.data<nullptr, &Command::entity>("entity"_hs)
+            .data<&Command::source>("source"_hs)
+            .data<&Command::target>("target"_hs)
+            //.ctor<decltype(Command::source), decltype(Command::target)>()
+        ;
+    }
+
+	template <>
     void reflect<PrintCommand>()
     {
         engine_command_type<PrintCommand>()
-            .base<Command>()
             .data<&PrintCommand::message>("message"_hs)
             .ctor<decltype(Command::source), decltype(Command::target), decltype(PrintCommand::message)>()
         ;
@@ -21,8 +34,7 @@ namespace engine
 	template <>
 	void reflect<ComponentPatchCommand>()
 	{
-		engine_meta_type<ComponentPatchCommand>()
-			.base<Command>()
+		engine_command_type<ComponentPatchCommand>()
 			.data<&ComponentPatchCommand::component>("component"_hs)
 			.ctor
 			<
@@ -36,8 +48,7 @@ namespace engine
 	template <>
 	void reflect<ComponentReplaceCommand>()
 	{
-		engine_meta_type<ComponentReplaceCommand>()
-			.base<Command>()
+		engine_command_type<ComponentReplaceCommand>()
 			.data<&ComponentReplaceCommand::component>("component"_hs)
 			.ctor
 			<
