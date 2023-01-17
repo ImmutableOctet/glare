eebd37fc9c6736da0a64ccf0cb04f64067ef85d7
Anthony Diamond <sonickid8@live.com>
2023-01-17 04:03:40 -0700

    Updated `MetaDescription::get_definition` to use `MetaTypeDescriptor::type_id`, etc.

--- glare/src/engine/meta/meta_description.cpp
+++ glare/src/engine/meta/meta_description.cpp
@@ -5,19 +5,19 @@ namespace engine
 {
 	const MetaTypeDescriptor* MetaDescription::get_definition(MetaType type) const
 	{
+		return get_definition(type.id());
+	}
+
+	const MetaTypeDescriptor* MetaDescription::get_definition(MetaTypeID type_id) const
+	{
 		for (const auto& type_desc : type_definitions)
 		{
-			if (type_desc.type == type)
+			if (type_desc.type_id == type_id)
 			{
 				return &type_desc;
 			}
 		}
 
 		return nullptr;
-	}
-
-	const MetaTypeDescriptor* MetaDescription::get_definition(MetaTypeID type_id) const
-	{
-		return get_definition(resolve(type_id));
 	}
 }
