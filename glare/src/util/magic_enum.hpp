1fcb7fecb5c28ec666faa7ad8445bc21a54240c5
Anthony Diamond <sonickid8@live.com>
2023-01-17 03:15:18 -0700

    Updated the include path for `magic_enum`.

--- glare/src/util/magic_enum.hpp
+++ glare/src/util/magic_enum.hpp
@@ -3,4 +3,5 @@
 #define MAGIC_ENUM_RANGE_MIN 0 // -128
 #define MAGIC_ENUM_RANGE_MAX 512 // 256 // 128
 
-#include <magic_enum/magic_enum.hpp>
+//#include <magic_enum/magic_enum.hpp>
+#include <magic_enum.hpp>
