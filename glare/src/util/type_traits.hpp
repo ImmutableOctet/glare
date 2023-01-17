3d95be8cfd1e9a4436db8aa7c5bcb804a0045ad6
Anthony Diamond <sonickid8@live.com>
2023-01-17 03:16:54 -0700

    Added traits for member pointers.

--- glare/src/util/type_traits.hpp
+++ glare/src/util/type_traits.hpp
@@ -9,4 +9,28 @@ namespace util
 {
 	template <typename ReturnType, typename Callable, typename ...Args>
 	constexpr bool is_return_value = std::is_same_v<std::invoke_result_t<Callable, Args...>, ReturnType>;
+
+	template<typename T>
+	struct parent_type_from_member_pointer;
+
+	template<typename Type, typename MemberType>
+	struct parent_type_from_member_pointer<MemberType Type::*>
+	{
+		using type = Type;
+		
+		using parent_type = Type;
+		using member_type = MemberType;
+	};
+
+	template<typename T>
+	struct member_pointer_type;
+
+	template<typename Type, typename MemberType>
+	struct member_pointer_type<MemberType Type::*>
+	{
+		using type = MemberType;
+
+		using parent_type = Type;
+		using member_type = MemberType;
+	};
 }
