b25712341d2067ff4eceae1a34f9ad02d352e02a
Anthony Diamond <sonickid8@live.com>
2023-01-17 04:22:30 -0700

    Reintroduced `StateComponent`.

---
+++ glare/src/engine/entity/components/state_component.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <engine/types.hpp>
+
+namespace engine
+{
+	// Indicates which state an entity is currently in.
+	struct StateComponent
+	{
+		EntityStateIndex state_index;
+		EntityStateIndex prev_state_index;
+
+		//EntityStateHash state_id;
+	};
+}
