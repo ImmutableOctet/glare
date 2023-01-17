ae56b0921a1ee2f1cc3788ea103a76224e12d8e4
Anthony Diamond <sonickid8@live.com>
2023-01-17 04:19:46 -0700

    Removed initial version of `StateSystem`.

--- glare/src/engine/state/components/frozen_state_component.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <engine/state/state_storage_manager.hpp>
-
-namespace engine
-{
-	// Used to encapsulate component instances that are (temporarily) excluded during a state.
-	struct FrozenStateComponent : public StateStorageManager {};
-}
--- glare/src/engine/state/components/state_storage_component.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <engine/state/state_storage_manager.hpp>
-
-namespace engine
-{
-	// Used to encapsulate component instances stored local to a state.
-	struct StateStorageComponent : public StateStorageManager {};
-}
