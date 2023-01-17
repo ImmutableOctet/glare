de5319a5b0af0f6c155ea20754d34d89eaa0f725
Anthony Diamond <sonickid8@live.com>
2023-01-17 05:27:36 -0700

    Implemented initial version of `EntityThread` type.

---
+++ glare/src/engine/entity/entity_thread.cpp
@@ -0,0 +1,98 @@
+#include "entity_thread.hpp"
+
+//#include <util/variant.hpp>
+
+namespace engine
+{
+	EntityThread::EntityThread
+	(
+		const EntityThreadFlags& flags,
+		EntityThreadIndex thread_index,
+		InstructionIndex first_instruction,
+		std::optional<EntityStateIndex> state_index
+	) :
+		EntityThreadFlags(flags),
+		thread_index(thread_index),
+		next_instruction(first_instruction),
+		state_index(state_index)
+	{}
+
+	bool EntityThread::pause()
+	{
+		const auto was_paused = is_paused;
+
+		is_paused = true;
+
+		return !was_paused;
+	}
+
+	bool EntityThread::resume()
+	{
+		const auto was_paused = is_paused;
+
+		is_paused = false;
+
+		return was_paused;
+	}
+
+	bool EntityThread::link()
+	{
+		const auto was_linked = is_linked;
+
+		is_linked = true;
+
+		return !was_linked;
+	}
+
+	bool EntityThread::unlink()
+	{
+		const auto was_linked = is_linked;
+
+		is_linked = false;
+
+		return was_linked;
+	}
+
+	bool EntityThread::attach(std::optional<EntityStateIndex> state_index, bool keep_existing_state)
+	{
+		const auto was_detached = is_detached;
+
+		is_detached = false;
+
+		if (state_index)
+		{
+			if (!keep_existing_state || !this->state_index.has_value())
+			{
+				this->state_index = *state_index;
+			}
+		}
+
+		return was_detached;
+	}
+
+	bool EntityThread::detach()
+	{
+		const auto was_detached = is_detached;
+
+		is_detached = true;
+
+		return !was_detached;
+	}
+
+	EntityInstructionCount EntityThread::skip(EntityInstructionCount forward_stride)
+	{
+		next_instruction += forward_stride;
+
+		return forward_stride;
+	}
+
+	EntityInstructionCount EntityThread::rewind(EntityInstructionCount backward_stride)
+	{
+		// Ensure we can actually move back the amount requested.
+		assert(next_instruction >= backward_stride);
+
+		next_instruction -= backward_stride;
+
+		return backward_stride;
+	}
+}
---
+++ glare/src/engine/entity/entity_thread.hpp
@@ -0,0 +1,96 @@
+#pragma once
+
+#include "types.hpp"
+#include "event_trigger_condition.hpp"
+
+#include <optional>
+
+namespace engine
+{
+	struct EntityThreadFlags
+	{
+		// If a thread is detached, it will continue to execute even when the active state changes.
+		// (By default, threads are terminated upon state change; i.e. attached)
+		//
+		// NOTES:
+		// 
+		// * A thread can be detached (meaning it'll run indefinitely), but still
+		//   be linked (meaning it can be referenced by name). This allows the user to control
+		//   when a thread stops execution, rather than being subjected to forced termination.
+		//
+		// * All threads (regardless of attachment or link status) are terminated when
+		//   the thread instance is destroyed or otherwise removed. (see: `EntityThreadComponent`)
+		//   
+		//   By definition, this means that a thread cannot outlive the entity that spawned it.
+		bool is_detached : 1 = false;
+
+		// By default, threads take on a 'linked' state, which ensures that
+		// only one instance of a given thread is executing per-entity.
+		//
+		// If a thread is unlinked, it will continue to execute until stopped/terminated, but will
+		// not block the creation of another instance pointing to the same `EntityThreadDescription` object.
+		//
+		// Linked threads are able to be referenced by name.
+		bool is_linked : 1 = true;
+
+		// Indicates if this thread has been paused.
+		// 
+		// A paused thread must be resumed by another thread (or rule-trigger) to proceed.
+		bool is_paused : 1 = false;
+
+		// Indicates if this thread is currently awaiting an event.
+		// 
+		// A thread that is yielding cannot continue execution
+		// until its trigger condition has been satisfied.
+		// 
+		// See also: `EntityListener`
+		bool is_yielding : 1 = false;
+
+		// Indicates whether this thread has reached a termination point.
+		bool is_complete : 1 = false;
+
+		// A thread is considered suspended if it cannot continue
+		// work until an external operation takes place.
+		// (e.g. a 'resume' operation or a yield-condition being met)
+		inline bool is_suspended() const
+		{
+			return (is_paused || is_yielding || is_complete);
+		}
+	};
+
+	struct EntityThread : public EntityThreadFlags // protected
+	{
+		public:
+			using InstructionIndex = EntityInstructionIndex;
+
+			EntityThread
+			(
+				const EntityThreadFlags& flags,
+
+				EntityThreadIndex thread_index,
+				InstructionIndex first_instruction=0,
+				std::optional<EntityStateIndex> state_index=std::nullopt
+			);
+
+			bool pause();
+			bool resume();
+			bool link();
+			bool unlink();
+			bool attach(std::optional<EntityStateIndex> state_index=std::nullopt, bool keep_existing_state=false);
+			bool detach();
+
+			EntityInstructionCount skip(EntityInstructionCount forward_stride);
+			EntityInstructionCount rewind(EntityInstructionCount backward_stride);
+
+			// An identifier used to locate the underlying
+			// `EntityThreadDescription` data for this thread.
+			EntityThreadIndex thread_index;
+
+			// An index pointing to the next instruction,
+			// found in `EntityThreadDescription`.
+			InstructionIndex next_instruction = 0;
+
+			// An index representing the state this thread was instantiated from.
+			std::optional<EntityStateIndex> state_index;
+	};
+}
