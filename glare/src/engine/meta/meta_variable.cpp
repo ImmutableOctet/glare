f5dd7e7a1ca51cced5ca40ef833c7add02882aca
Anthony Diamond <sonickid8@live.com>
2023-01-20 05:17:20 -0700

    Added the ability to specify a `ParsingContext` when constructing a `MetaVariable` object. (Used for value resolution)

--- glare/src/engine/meta/meta_variable.cpp
+++ glare/src/engine/meta/meta_variable.cpp
@@ -10,26 +10,26 @@ namespace engine
 	MetaVariable::MetaVariable(std::string_view name, entt::meta_any&& value)
 		: name(hash(name)), value(std::move(value)) {}
 
-	MetaVariable::MetaVariable(MetaSymbolID name, const util::json& value, const Instructions& instructions)
-		: MetaVariable(name, resolve_meta_any(value, instructions))
+	MetaVariable::MetaVariable(MetaSymbolID name, const util::json& value, const Instructions& instructions, const ParsingContext* opt_parsing_context)
+		: MetaVariable(name, resolve_meta_any(value, instructions, opt_parsing_context))
 	{}
 
-	MetaVariable::MetaVariable(MetaSymbolID name, const util::json& value, MetaType type, const Instructions& instructions)
-		: MetaVariable(name, resolve_meta_any(value, type, instructions))
+	MetaVariable::MetaVariable(MetaSymbolID name, const util::json& value, MetaType type, const Instructions& instructions, const ParsingContext* opt_parsing_context)
+		: MetaVariable(name, resolve_meta_any(value, type, instructions, opt_parsing_context))
 	{}
 
-	MetaVariable::MetaVariable(MetaSymbolID name, const util::json& value, MetaTypeID type_id, const Instructions& instructions)
-		: MetaVariable(name, resolve_meta_any(value, type_id, instructions))
+	MetaVariable::MetaVariable(MetaSymbolID name, const util::json& value, MetaTypeID type_id, const Instructions& instructions, const ParsingContext* opt_parsing_context)
+		: MetaVariable(name, resolve_meta_any(value, type_id, instructions, opt_parsing_context))
 	{}
 
-	MetaVariable::MetaVariable(std::string_view name, const util::json& value, const Instructions& instructions)
-		: MetaVariable(hash(name), value, instructions) {}
+	MetaVariable::MetaVariable(std::string_view name, const util::json& value, const Instructions& instructions, const ParsingContext* opt_parsing_context)
+		: MetaVariable(hash(name), value, instructions, opt_parsing_context) {}
 
-	MetaVariable::MetaVariable(std::string_view name, const util::json& value, MetaType type, const Instructions& instructions)
-		: MetaVariable(hash(name), value, type, instructions)
+	MetaVariable::MetaVariable(std::string_view name, const util::json& value, MetaType type, const Instructions& instructions, const ParsingContext* opt_parsing_context)
+		: MetaVariable(hash(name), value, type, instructions, opt_parsing_context)
 	{}
 
-	MetaVariable::MetaVariable(std::string_view name, const util::json& value, MetaTypeID type_id, const Instructions& instructions)
-		: MetaVariable(hash(name), value, type_id, instructions)
+	MetaVariable::MetaVariable(std::string_view name, const util::json& value, MetaTypeID type_id, const Instructions& instructions, const ParsingContext* opt_parsing_context)
+		: MetaVariable(hash(name), value, type_id, instructions, opt_parsing_context)
 	{}
 }
