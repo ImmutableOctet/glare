6cd5c7fc7de525a359dc41522bf4f34d0f1c743f
Anthony Diamond <sonickid8@live.com>
2023-01-23 03:57:23 -0700

    Added reflection for the different light component types, updated reflection for `CameraComponent`, etc.

--- glare/src/engine/world/reflection.cpp
+++ glare/src/engine/world/reflection.cpp
@@ -8,6 +8,11 @@
 
 #include "components/camera_component.hpp"
 #include "components/light_component.hpp"
+#include "components/directional_light_component.hpp"
+#include "components/directional_light_shadow_component.hpp"
+#include "components/point_light_component.hpp"
+#include "components/point_light_shadow_component.hpp"
+#include "components/spot_light_component.hpp"
 
 #include <engine/components/model_component.hpp>
 #include <graphics/model.hpp>
@@ -20,10 +25,10 @@ namespace engine
 	namespace world_impl
 	{
 		template <typename T>
-		void extend() {}
+		static void extend() {}
 
 		template <>
-		inline void extend<ModelComponent>()
+		static void extend<ModelComponent>()
 		{
 			entt::meta<ModelComponent>()
 				.func<&attach_model>("attach_model"_hs)
@@ -34,44 +39,61 @@ namespace engine
 			;
 		}
 
-		void reflect_extensions()
+		static void reflect_extensions()
 		{
 			extend<ModelComponent>();
+		}
+
+		template <typename ShadowComponentType>
+		static void reflect_shadow_component()
+		{
+			engine_meta_type<ShadowComponentType>()
+				.data<&ShadowComponentType::shadow_perspective>("shadow_perspective"_hs)
+				.data<&ShadowComponentType::transforms>("transforms"_hs)
+				//.data<&ShadowComponentType::shadow_map>("shadow_map"_hs)
+				
+				/*
+				.ctor
+				<
+					decltype(ShadowComponentType::shadow_perspective),
+					decltype(ShadowComponentType::transforms),
+					decltype(ShadowComponentType::shadow_map)&&
+				>()
+				*/
+
+				//.ctor<...>()
+			;
 		}
 	}
 
 	template <>
 	void reflect<CameraComponent>()
 	{
-		engine_meta_type<CameraComponent>()
-			.data<&CameraComponent::fov>("fov"_hs)
-			.data<&CameraComponent::aspect_ratio>("aspect_ratio"_hs)
+		auto camera_component = engine_meta_type<CameraComponent>()
+			//.data<&CameraComponent::fov>("fov"_hs)
+			.data<&CameraComponent::set_vertical_fov, &CameraComponent::get_vertical_fov>("fov"_hs)
+
 			.data<&CameraComponent::near_plane>("near_plane"_hs)
 			.data<&CameraComponent::far_plane>("far_plane"_hs)
+			.data<&CameraComponent::aspect_ratio>("aspect_ratio"_hs)
 			.data<&CameraComponent::projection_mode>("projection_mode"_hs)
 
-			// Getters/setters:
 			.data<&CameraComponent::set_free_rotation, &CameraComponent::get_free_rotation>("free_rotation"_hs)
 			.data<&CameraComponent::set_dynamic_aspect_ratio, &CameraComponent::get_dynamic_aspect_ratio>("dynamic_aspect_ratio"_hs)
 
+			//.data<&CameraComponent::set_horizontal_fov, &CameraComponent::get_horizontal_fov>("h_fov"_hs)
+			//.data<&CameraComponent::set_vertical_fov, &CameraComponent::get_vertical_fov>("v_fov"_hs)
+
 			.ctor
 			<
 				decltype(CameraComponent::fov),
-				decltype(CameraComponent::aspect_ratio),
+				
 				decltype(CameraComponent::near_plane),
 				decltype(CameraComponent::far_plane),
+				decltype(CameraComponent::aspect_ratio),
 				decltype(CameraComponent::projection_mode),
 				
 				bool, bool
-			>()
-			
-			.ctor
-			<
-				decltype(CameraComponent::fov),
-				decltype(CameraComponent::aspect_ratio),
-				decltype(CameraComponent::near_plane),
-				decltype(CameraComponent::far_plane),
-				decltype(CameraComponent::projection_mode)
 			>()
 		;
 	}
@@ -83,6 +105,9 @@ namespace engine
 			.data<&LightProperties::ambient>("ambient"_hs)
 			.data<&LightProperties::diffuse>("diffuse"_hs)
 			.data<&LightProperties::specular>("specular"_hs)
+			
+			//.data<nullptr, &LightProperties::max_brightness>("max_brightness"_hs)
+
 			.ctor
 			<
 				decltype(LightProperties::ambient),
@@ -97,6 +122,8 @@ namespace engine
 	{
 		engine_meta_type<DirectionalLightComponent>()
 			.data<&DirectionalLightComponent::set_use_position, &DirectionalLightComponent::get_use_position>("use_position"_hs)
+
+			.ctor<decltype(DirectionalLightComponent::use_position)>()
 		;
 	}
 
@@ -104,23 +131,17 @@ namespace engine
 	void reflect<SpotLightComponent>()
 	{
 		engine_meta_type<SpotLightComponent>()
-			.data<&SpotLightComponent::cutoff>("cutoff"_hs)
-			.data<&SpotLightComponent::outer_cutoff>("outer_cutoff"_hs)
 			.data<&SpotLightComponent::constant>("constant"_hs)
 			.data<&SpotLightComponent::linear>("linear"_hs)
 			.data<&SpotLightComponent::quadratic>("quadratic"_hs)
+			.data<&SpotLightComponent::cutoff>("cutoff"_hs)
+			.data<&SpotLightComponent::outer_cutoff>("outer_cutoff"_hs)
 
 			.ctor
 			<
-				decltype(SpotLightComponent::cutoff),
-				decltype(SpotLightComponent::outer_cutoff),
 				decltype(SpotLightComponent::constant),
 				decltype(SpotLightComponent::linear),
-				decltype(SpotLightComponent::quadratic)
-			>()
-
-			.ctor
-			<
+				decltype(SpotLightComponent::quadratic),
 				decltype(SpotLightComponent::cutoff),
 				decltype(SpotLightComponent::outer_cutoff)
 			>()
@@ -131,11 +152,13 @@ namespace engine
 	void reflect<PointLightComponent>()
 	{
 		engine_meta_type<PointLightComponent>()
+			.data<&PointLightComponent::constant>("constant"_hs)
 			.data<&PointLightComponent::linear>("linear"_hs)
 			.data<&PointLightComponent::quadratic>("quadratic"_hs)
 
 			.ctor
 			<
+				decltype(PointLightComponent::constant),
 				decltype(PointLightComponent::linear),
 				decltype(PointLightComponent::quadratic)
 			>()
@@ -148,19 +171,43 @@ namespace engine
 	template <>
 	void reflect<LightComponent>()
 	{
-		// TODO: Migrate this to `engine`'s `reflect_primitives` subroutine.
-		reflect<LightProperties>();
-
 		engine_meta_type<LightComponent>()
-			.data<&LightComponent::type>("type"_hs)
-			.data<&LightComponent::properties>("properties"_hs)
-			.func<&LightComponent::resolve_light_mode>("resolve_light_mode"_hs)
-			.ctor<decltype(LightComponent::type), decltype(LightComponent::properties)>()
-		;
+			.base<LightProperties>()
 
-		reflect<DirectionalLightComponent>();
-		reflect<SpotLightComponent>();
-		reflect<PointLightComponent>();
+			.data<&LightComponent::type>("type"_hs)
+			.data<&LightComponent::set_casts_shadows, &LightComponent::get_casts_shadows>("casts_shadows"_hs)
+			
+			.ctor
+			<
+				decltype(LightProperties::ambient),
+				decltype(LightProperties::diffuse),
+				decltype(LightProperties::specular),
+				
+				decltype(LightComponent::type)
+			>()
+
+			.ctor
+			<
+				decltype(LightProperties::ambient),
+				decltype(LightProperties::diffuse),
+				decltype(LightProperties::specular),
+				
+				decltype(LightComponent::type),
+				decltype(LightComponent::casts_shadows)
+			>()
+		;
+	}
+
+	template <>
+	void reflect<DirectionalLightShadowComponent>()
+	{
+		world_impl::reflect_shadow_component<DirectionalLightShadowComponent>();
+	}
+
+	template <>
+	void reflect<PointLightShadowComponent>()
+	{
+		world_impl::reflect_shadow_component<PointLightShadowComponent>();
 	}
 
 	template <>
@@ -182,12 +229,25 @@ namespace engine
 	template <>
 	void reflect<World>()
 	{
-		reflect<CameraComponent>();
-		reflect<LightComponent>();  // NOTE: Handles several lighting-related types.
+		reflect<CameraProjection>();
 		reflect<WorldProperties>();
+		reflect<LightProperties>();
+
+		// Components and behaviors:
+		reflect<CameraComponent>();
+
+		// NOTE: Handles several lighting-related types.
+		reflect<LightComponent>();
+		reflect<DirectionalLightComponent>();
+		reflect<SpotLightComponent>();
+		reflect<PointLightComponent>();
+
+		reflect<DirectionalLightShadowComponent>();
+		reflect<PointLightShadowComponent>();
 
 		reflect_behaviors();
 
+		// Systems:
 		reflect<AnimationSystem>();
 		reflect<PhysicsSystem>();
 		reflect<MotionSystem>();
