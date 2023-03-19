46f0a9c23af9c7eeb193eb956b7fe70896b8fe38
Anthony Diamond <sonickid8@live.com>
2023-03-19 02:19:00 -0600

    Added test for `util::is_whitespace`.

--- test/src/util/string.cpp
+++ test/src/util/string.cpp
@@ -12,6 +12,14 @@ TEST_CASE("util::is_quoted", "[util:string]")
 	REQUIRE(!util::is_quoted(std::string_view("\"Test\\\"")));
 	REQUIRE(!util::is_quoted(std::string_view("Test")));
 }
+TEST_CASE("util::is_whitespace", "[util:string]")
+{
+	REQUIRE(util::is_whitespace(' '));
+	REQUIRE(!util::is_whitespace('A'));
+	REQUIRE(util::is_whitespace("     \t\t\n  \t\n"));
+	REQUIRE(!util::is_whitespace("    \t \n  \t\tabc\t\n"));
+	REQUIRE(!util::is_whitespace("Nowhitespacecharactersatall"));
+}
 
 TEST_CASE("util:string", "[util]")
 {
