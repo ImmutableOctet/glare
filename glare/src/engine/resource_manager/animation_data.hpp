85558f1a84a6c30323e3ace24815cabce50d61f4
Anthony Diamond <sonickid8@live.com>
2023-03-19 03:30:00 -0600

    Explicit includes for `vector` and `map`.

--- glare/src/engine/resource_manager/animation_data.hpp
+++ glare/src/engine/resource_manager/animation_data.hpp
@@ -6,6 +6,9 @@
 #include <graphics/animation.hpp>
 #include <graphics/skeleton.hpp>
 
+#include <vector>
+#include <map>
+
 namespace engine
 {
 	using Animations = std::vector<graphics::Animation>;
