26bd6507fa57745056809f4b11730c1e07f7d157
Anthony Diamond <sonickid8@live.com>
2023-03-19 02:20:57 -0600

    Added tests for `util::split`.

--- test/src/util/string.cpp
+++ test/src/util/string.cpp
@@ -2,6 +2,8 @@
 #include <util/string.hpp>
 
 #include <cstdint>
+#include <vector>
+
 TEST_CASE("util::is_quoted", "[util:string]")
 {
 	REQUIRE(!util::is_quoted(std::string_view("\"Test\" == \"Test\"")));
@@ -32,17 +34,175 @@ TEST_CASE("util::trim", "[util:string]")
 	REQUIRE(util::trim("some::values::etc") == "some::values::etc");
 }
 
-TEST_CASE("util:string", "[util]")
+TEST_CASE("util::split", "[util:string]")
 {
-	SECTION("from_string")
+	SECTION("Normal use-case")
 	{
-		// Empty command:
-		{
-			REQUIRE(util::from_string<std::int32_t>("1"));
-			REQUIRE(util::from_string<float>("1.5"));
-			REQUIRE(!util::from_string<std::int32_t>("1.0", true));
-			REQUIRE(util::from_string<std::int32_t>("1.0", false).value_or(0) == 1);
-			REQUIRE(util::from_string<std::int32_t>("1.35", false).value_or(0) == 1);
-		}
+		std::string_view content = "Some::String.With->Separators";
+
+		std::vector<std::string_view> result;
+
+		util::split
+		(
+			content,
+
+			std::array
+			{
+				std::string_view { "::" },
+				std::string_view { "."  },
+				std::string_view { "->" }
+			},
+
+			[&result](std::string_view symbol, bool is_last_symbol) -> bool
+			{
+				result.emplace_back(symbol);
+
+				return true;
+			}
+		);
+
+		REQUIRE(result.size() == 4);
+		REQUIRE(result[0] == "Some");
+		REQUIRE(result[1] == "String");
+		REQUIRE(result[2] == "With");
+		REQUIRE(result[3] == "Separators");
 	}
+
+	SECTION("Stop execution early")
+	{
+		std::string_view content = "First message::something else|etc";
+
+		std::string_view first_result;
+
+		util::split
+		(
+			content,
+
+			std::array
+			{
+				std::string_view { "::" },
+				std::string_view { "|"  }
+			},
+
+			[&first_result](std::string_view symbol, bool is_last_symbol) -> bool
+			{
+				first_result = symbol;
+
+				return false;
+			}
+		);
+
+		REQUIRE(!first_result.empty());
+		REQUIRE(first_result == "First message");
+	}
+
+	SECTION("Empty string between separators")
+	{
+		std::string_view content = "X..Y::::Z";
+
+		std::vector<std::string_view> result;
+
+		util::split
+		(
+			content,
+
+			std::array
+			{
+				std::string_view { "::" },
+				std::string_view { "."  }
+			},
+
+			[&result](std::string_view symbol, bool is_last_symbol) -> bool
+			{
+				result.emplace_back(symbol);
+
+				return true;
+			}
+		);
+
+		REQUIRE(result.size() == 3);
+		REQUIRE(result[0] == "X");
+		REQUIRE(result[1] == "Y");
+		REQUIRE(result[2] == "Z");
+	}
+
+	SECTION("String beginning with separators")
+	{
+		std::string_view content = "...X::Y";
+
+		std::vector<std::string_view> result;
+
+		util::split
+		(
+			content,
+
+			std::array
+			{
+				std::string_view { "::" },
+				std::string_view { "."  }
+			},
+
+			[&result](std::string_view symbol, bool is_last_symbol) -> bool
+			{
+				result.emplace_back(symbol);
+
+				return true;
+			}
+		);
+
+		REQUIRE(result.size() == 2);
+		REQUIRE(result[0] == "X");
+		REQUIRE(result[1] == "Y");
+	}
+
+	SECTION("Callback alters `string_view` slice")
+	{
+		std::string_view content = "...fn(X::Y)::Z";
+
+		std::vector<std::string_view> result;
+
+		util::split
+		(
+			content,
+
+			std::array
+			{
+				std::string_view { "::" },
+				std::string_view { "."  }
+			},
+
+			[&content, &result](std::string_view& symbol, bool is_last_symbol) -> bool
+			{
+				if (auto call_begin = symbol.find('('); call_begin != std::string_view::npos)
+				{
+					const auto call_position_in_content = static_cast<std::size_t>((symbol.data() + call_begin) - content.data());
+
+					if (auto call_end_position_in_content = content.find(')', call_position_in_content); call_end_position_in_content != std::string_view::npos)
+					{
+						const auto symbol_position_in_content = (symbol.data() - content.data());
+						const auto call_expr_length = ((call_end_position_in_content + 1) - symbol_position_in_content);
+
+						symbol = content.substr(symbol_position_in_content, call_expr_length);
+					}
+				}
+
+				result.emplace_back(symbol);
+
+				return true;
+			}
+		);
+
+		REQUIRE(result.size() == 2);
+		REQUIRE(result[0] == "fn(X::Y)");
+		REQUIRE(result[1] == "Z");
+	}
+}
+
+TEST_CASE("util::from_string", "[util:string]")
+{
+	REQUIRE(util::from_string<std::int32_t>("1"));
+	REQUIRE(util::from_string<float>("1.5"));
+	REQUIRE(!util::from_string<std::int32_t>("1.0", true));
+	REQUIRE(util::from_string<std::int32_t>("1.0", false).value_or(0) == 1);
+	REQUIRE(util::from_string<std::int32_t>("1.35", false).value_or(0) == 1);
 }
