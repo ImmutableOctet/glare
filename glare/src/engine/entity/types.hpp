61b782212e801adc6eb90fc5cfcd6b19f3238492
Anthony Diamond <sonickid8@live.com>
2023-03-20 14:54:59 -0600

    Added `EntityStateCount` alias.

--- glare/src/engine/entity/types.hpp
+++ glare/src/engine/entity/types.hpp
@@ -15,6 +15,8 @@ namespace engine
 	// Identifier used to represent an index of an entity state.
 	using EntityStateIndex = std::uint16_t; // std::uint8_t;
 
+	using EntityStateCount = EntityStateIndex; // std::uint16_t; // std::uint8_t;
+
 	using EntityStateID = StringHash;
 
 	using EntityInstructionIndex = std::uint16_t; // std::uint32_t; // std::size_t;
