517b40d4bbc3d99d93478a4b9edfc191dc355152
Anthony Diamond <sonickid8@live.com>
2023-03-20 15:07:10 -0600

    Various tweaks related to new parsing context functionality, changed some direct uses of `entt::meta_any` in favor of `MetaAny` alias, etc.

--- glare/src/engine/meta/meta_variable.cpp
+++ glare/src/engine/meta/meta_variable.cpp
@@ -4,32 +4,32 @@
 
 namespace engine
 {
-	MetaVariable::MetaVariable(MetaSymbolID name, entt::meta_any&& value)
+	MetaVariable::MetaVariable(MetaSymbolID name, MetaAny&& value)
 		: name(name), value(std::move(value)) {}
 
-	MetaVariable::MetaVariable(std::string_view name, entt::meta_any&& value)
+	MetaVariable::MetaVariable(std::string_view name, MetaAny&& value)
 		: name(hash(name)), value(std::move(value)) {}
 
-	MetaVariable::MetaVariable(MetaSymbolID name, const util::json& value, const Instructions& instructions, const ParsingContext* opt_parsing_context)
-		: MetaVariable(name, resolve_meta_any(value, instructions, opt_parsing_context))
+	MetaVariable::MetaVariable(MetaSymbolID name, const util::json& value, const Instructions& instructions)
+		: MetaVariable(name, resolve_meta_any(value, instructions))
 	{}
 
-	MetaVariable::MetaVariable(MetaSymbolID name, const util::json& value, MetaType type, const Instructions& instructions, const ParsingContext* opt_parsing_context)
-		: MetaVariable(name, resolve_meta_any(value, type, instructions, opt_parsing_context))
+	MetaVariable::MetaVariable(MetaSymbolID name, const util::json& value, MetaType type, const Instructions& instructions)
+		: MetaVariable(name, resolve_meta_any(value, type, instructions))
 	{}
 
-	MetaVariable::MetaVariable(MetaSymbolID name, const util::json& value, MetaTypeID type_id, const Instructions& instructions, const ParsingContext* opt_parsing_context)
-		: MetaVariable(name, resolve_meta_any(value, type_id, instructions, opt_parsing_context))
+	MetaVariable::MetaVariable(MetaSymbolID name, const util::json& value, MetaTypeID type_id, const Instructions& instructions)
+		: MetaVariable(name, resolve_meta_any(value, type_id, instructions))
 	{}
 
-	MetaVariable::MetaVariable(std::string_view name, const util::json& value, const Instructions& instructions, const ParsingContext* opt_parsing_context)
-		: MetaVariable(hash(name), value, instructions, opt_parsing_context) {}
+	MetaVariable::MetaVariable(std::string_view name, const util::json& value, const Instructions& instructions)
+		: MetaVariable(hash(name), value, instructions) {}
 
-	MetaVariable::MetaVariable(std::string_view name, const util::json& value, MetaType type, const Instructions& instructions, const ParsingContext* opt_parsing_context)
-		: MetaVariable(hash(name), value, type, instructions, opt_parsing_context)
+	MetaVariable::MetaVariable(std::string_view name, const util::json& value, MetaType type, const Instructions& instructions)
+		: MetaVariable(hash(name), value, type, instructions)
 	{}
 
-	MetaVariable::MetaVariable(std::string_view name, const util::json& value, MetaTypeID type_id, const Instructions& instructions, const ParsingContext* opt_parsing_context)
-		: MetaVariable(hash(name), value, type_id, instructions, opt_parsing_context)
+	MetaVariable::MetaVariable(std::string_view name, const util::json& value, MetaTypeID type_id, const Instructions& instructions)
+		: MetaVariable(hash(name), value, type_id, instructions)
 	{}
 }
--- glare/src/engine/meta/meta_variable.hpp
+++ glare/src/engine/meta/meta_variable.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "types.hpp"
+#include "meta_parsing_instructions.hpp"
 
 // TODO: Forward declare JSON type.
 #include <util/json.hpp>
@@ -9,32 +10,32 @@
 
 namespace engine
 {
-	struct ParsingContext;
+	class MetaParsingContext;
 
 	// Wrapper type around a name + meta_any value; used to represent a definition of a variable.
 	struct MetaVariable
 	{
-		using Instructions = MetaAnyParseInstructions;
+		using Instructions = MetaParsingInstructions;
 
-		MetaVariable(MetaSymbolID name, entt::meta_any&& value);
-		MetaVariable(std::string_view name, entt::meta_any&& value);
+		MetaVariable(MetaSymbolID name, MetaAny&& value);
+		MetaVariable(std::string_view name, MetaAny&& value);
 		
 		// NOTE: This overload does not allow for non-primitive type resolution.
 		// (see overload taking `type_id`)
-		MetaVariable(MetaSymbolID name, const util::json& value, const Instructions& instructions={}, const ParsingContext* opt_parsing_context=nullptr);
+		MetaVariable(MetaSymbolID name, const util::json& value, const Instructions& instructions={});
 
-		MetaVariable(MetaSymbolID name, const util::json& value, MetaType type, const Instructions& instructions={}, const ParsingContext* opt_parsing_context=nullptr);
-		MetaVariable(MetaSymbolID name, const util::json& value, MetaTypeID type_id, const Instructions& instructions={}, const ParsingContext* opt_parsing_context=nullptr);
+		MetaVariable(MetaSymbolID name, const util::json& value, MetaType type, const Instructions& instructions={});
+		MetaVariable(MetaSymbolID name, const util::json& value, MetaTypeID type_id, const Instructions& instructions={});
 
 		// Utility overload that automates hash generation of `name`.
 		// (see notes on primitive type limitations)
-		MetaVariable(std::string_view name, const util::json& value, const Instructions& instructions={}, const ParsingContext* opt_parsing_context=nullptr);
+		MetaVariable(std::string_view name, const util::json& value, const Instructions& instructions={});
 
 		// Utility overload that automates hash generation of `name`.
-		MetaVariable(std::string_view name, const util::json& value, MetaTypeID type_id, const Instructions& instructions={}, const ParsingContext* opt_parsing_context=nullptr);
+		MetaVariable(std::string_view name, const util::json& value, MetaTypeID type_id, const Instructions& instructions={});
 
 		// Utility overload that automates hash generation of `name`.
-		MetaVariable(std::string_view name, const util::json& value, MetaType type, const Instructions& instructions={}, const ParsingContext* opt_parsing_context=nullptr);
+		MetaVariable(std::string_view name, const util::json& value, MetaType type, const Instructions& instructions={});
 
 		MetaVariable(const MetaVariable&) = default;
 		MetaVariable(MetaVariable&&) noexcept = default;
