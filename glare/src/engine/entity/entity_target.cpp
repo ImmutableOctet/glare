fcdcdd7425208f8b1f3ad5fcd42074393d6b7deb
Anthony Diamond <sonickid8@live.com>
2023-03-20 03:43:54 -0600

    Significant improvements to `EntityTarget` implementation, integration with new expression-evaluation systems, etc.

--- glare/src/engine/entity/entity_target.cpp
+++ glare/src/engine/entity/entity_target.cpp
@@ -1,6 +1,9 @@
 #include "entity_target.hpp"
 
+#include "components/instance_component.hpp"
+
 #include <engine/meta/meta.hpp>
+#include <engine/meta/serial.hpp>
 
 #include <engine/components/relationship_component.hpp>
 #include <engine/components/name_component.hpp>
@@ -12,8 +15,72 @@
 
 namespace engine
 {
+	// EntityTarget:
+	static Entity find_child_impl(Registry& registry, Entity source, StringHash child_name, bool recursive=true)
+	{
+		if (!child_name)
+		{
+			return null;
+		}
+
+		const RelationshipComponent* relationship = registry.try_get<RelationshipComponent>(source);
+
+		if (!relationship)
+		{
+			return null;
+		}
+
+		Entity target_entity = null;
+
+		relationship->enumerate_children
+		(
+			registry,
+									
+			[&registry, child_name, &target_entity](Entity child, const RelationshipComponent& child_relationship, Entity next_child) -> bool
+			{
+				const auto* child_name_comp = registry.try_get<NameComponent>(child);
+
+				if (!child_name_comp)
+				{
+					return true;
+				}
+
+				if (child_name_comp->hash() == child_name)
+				{
+					target_entity = child;
+
+					return false;
+				}
+										
+				return true;
+			},
+
+			recursive
+		);
+
+		return target_entity;
+	}
+
 	Entity EntityTarget::resolve(Registry& registry, Entity source) const
 	{
+		return get(registry, source);
+	}
+
+	Entity EntityTarget::get(Registry& registry, Entity source) const
+	{
+		return get_impl(registry, source);
+	}
+
+	Entity EntityTarget::get(Registry& registry, Entity source, const MetaEvaluationContext& context) const
+	{
+		return get_impl(registry, source, context);
+	}
+
+	template <typename ...Args>
+	Entity EntityTarget::get_impl(Registry& registry, Entity source, Args&&... args) const
+	{
+		using namespace engine::literals;
+
 		Entity target_entity = null;
 
 		util::visit
@@ -38,9 +105,19 @@ namespace engine
 				target_entity = exact_entity.entity;
 			},
 
-			[&registry, &target_entity](const EntityNameTarget& named_target)
+			[&registry, source, &target_entity](const EntityNameTarget& named_target)
 			{
 				auto named_range = registry.view<NameComponent>().each();
+
+				if (named_target.search_children_first)
+				{
+					if (auto child = find_child_impl(registry, source, named_target.entity_name, false); (child != null)) // true
+					{
+						target_entity = child;
+
+						return;
+					}
+				}
 
 				for (auto it = named_range.begin(); it != named_range.end(); it++)
 				{
@@ -60,36 +137,10 @@ namespace engine
 			{
 				//assert(child_target.child_name);
 
-				const RelationshipComponent* relationship = registry.try_get<RelationshipComponent>(source);
-
-				if (!relationship)
-				{
-					return;
-				}
-
-				relationship->enumerate_children
+				target_entity = find_child_impl
 				(
-					registry,
-									
-					[&registry, &child_target, &target_entity](Entity child, const RelationshipComponent& child_relationship, Entity next_child) -> bool
-					{
-						const auto* child_name_comp = registry.try_get<NameComponent>(child);
-
-						if (!child_name_comp)
-						{
-							return true;
-						}
-
-						if (child_name_comp->hash() == child_target.child_name)
-						{
-							target_entity = child;
-
-							return false;
-						}
-										
-						return true;
-					},
-
+					registry, source,
+					child_target.child_name,
 					child_target.recursive
 				);
 			},
@@ -112,6 +163,31 @@ namespace engine
 				}
 			},
 
+			[&](const IndirectTarget& indirect_target)
+			{
+				auto* instance_comp = registry.try_get<InstanceComponent>(source);
+
+				if (!instance_comp)
+				{
+					return;
+				}
+
+				auto deferred_expr = indirect_target.target_value_expr.get(instance_comp->get_storage());
+
+				if (!deferred_expr)
+				{
+					return;
+				}
+
+				if (auto result = get_indirect_value_or_ref(deferred_expr, registry, source, args...))
+				{
+					if (auto result_as_entity = result.try_cast<EntityTarget>())
+					{
+						target_entity = result_as_entity->get(registry, source, args...);
+					}
+				}
+			},
+
 			[](const NullTarget&) // [&target_entity]
 			{
 				// Not actually required; defaults to `null` anyway.
@@ -122,20 +198,9 @@ namespace engine
 		return target_entity;
 	}
 
-	// TODO: Remove. (Workaround for `std::regex`)
-	std::optional<EntityTarget::ParseResult> EntityTarget::parse_type(std::string_view raw_value)
+	std::optional<EntityTarget::ParseResult> EntityTarget::parse_type(std::string_view raw_value, const MetaParsingInstructions* opt_parsing_instructions)
 	{
-		if (raw_value.empty())
-		{
-			return std::nullopt;
-		}
-
-		return parse_type(std::string(raw_value));
-	}
-
-	std::optional<EntityTarget::ParseResult> EntityTarget::parse_type(const std::string& raw_value)
-	{
-		using namespace entt::literals;
+		using namespace engine::literals;
 
 		if (raw_value.empty())
 		{
@@ -162,90 +227,147 @@ namespace engine
 			return ParseResult { NullTarget {}, (sizeof("null")-1), true };
 		}
 
-		if (auto [command_name, command_content, trailing_expr, is_string_content] = util::parse_single_argument_command(raw_value); !command_name.empty())
+		if (auto [command_name, command_content, trailing_expr, is_string_content, command_parsed_length] = util::parse_command(raw_value); !command_name.empty())
 		{
 			// NOTE: Workaround to avoid additional temporary string allocation. (`std::regex` limitation)
-			const auto parse_offset = static_cast<std::size_t>((command_content.data() + command_content.size() + (sizeof(")")-1)) - raw_value.data());
+			const auto parse_offset = (trailing_expr.empty())
+				? raw_value.length()
+				: static_cast<std::size_t>(trailing_expr.data() - raw_value.data())
+			;
 
 			// The above line would normally would look like this:
 			//const auto parse_offset = static_cast<std::size_t>((command_content.end() + sizeof(")")) - raw_value.begin()));
+
+			auto command_content_as_indirect = [&opt_parsing_instructions, &command_content, parse_offset]() -> std::optional<ParseResult>
+			{
+				if (command_content.empty())
+				{
+					return std::nullopt;
+				}
+
+				if (opt_parsing_instructions && opt_parsing_instructions->storage)
+				{
+					auto indirect_value = meta_any_from_string
+					(
+						command_content,
+						*opt_parsing_instructions,
+						engine::resolve<EntityTarget>(),
+						false, false, false
+					);
+
+					if (indirect_value)
+					{
+						const auto indirect_value_type = indirect_value.type();
+
+						if (auto as_indirect_meta_any = indirect_value.try_cast<IndirectMetaAny>())
+						{
+							return ParseResult { IndirectTarget { std::move(*as_indirect_meta_any) }, parse_offset, false };
+						}
+					}
+				}
+
+				return std::nullopt;
+			};
 
 			switch (hash(command_name))
 			{
 				case "entity"_hs:
 					if (!is_string_content)
 					{
-						if (auto entity_id_raw = util::from_string<entt::id_type>(raw_value))
+						if (auto entity_id_raw = util::from_string<entt::id_type>(command_content))
 						{
 							return ParseResult { ExactEntityTarget { static_cast<Entity>(*entity_id_raw) }, parse_offset, false };
 						}
+						else if (auto as_indirect = command_content_as_indirect())
+						{
+							return *as_indirect;
+						}
+					}
+
+					if (!command_content.empty())
+					{
+						return ParseResult { EntityNameTarget { hash(command_content) }, parse_offset, false };
 					}
 
 					break;
 
 				case "child"_hs:
-				{
-					if (command_content.contains(','))
+					if (!command_content.empty())
 					{
-						auto parameter_idx = 0;
-
-						std::string_view child_name;
-						bool recursive = true;
-
-						util::split(command_content, ",", [&parameter_idx, &child_name, &recursive](std::string_view argument, bool is_last_argument=false) -> bool
+						if (auto as_indirect = command_content_as_indirect())
 						{
-							argument = util::trim(argument);
+							return *as_indirect;
+						}
+						else if (command_content.contains(','))
+						{
+							auto parameter_idx = 0;
 
-							switch (parameter_idx++)
+							std::string_view child_name;
+							bool recursive = true;
+
+							util::split(command_content, ",", [&parameter_idx, &child_name, &recursive](std::string_view argument, bool is_last_argument=false) -> bool
 							{
-								// First argument: `child_name`
-								case 0:
-									child_name = argument;
+								argument = util::trim(argument);
 
-									break;
-
-								// Second argument: `recursive`
-								case 1:
+								switch (parameter_idx++)
 								{
-									switch (hash(argument))
+									// First argument: `child_name`
+									case 0:
+										child_name = argument;
+
+										break;
+
+									// Second argument: `recursive`
+									case 1:
 									{
-										// Recursion is enabled by default; no
-										// need to check for positive values:
-										/*
-										case "true"_hs:
-										case "1"_hs:
-											recursive = true;
+										switch (hash(argument))
+										{
+											// Recursion is enabled by default; no
+											// need to check for positive values:
+											/*
+											case "true"_hs:
+											case "1"_hs:
+												recursive = true;
 
-											break;
-										*/
+												break;
+											*/
 
-										//case "f"_hs:
-										case "0"_hs:
-										case "false"_hs:
-											recursive = false;
+											//case "f"_hs:
+											case "0"_hs:
+											case "false"_hs:
+												recursive = false;
 
-											break;
+												break;
+										}
+
+										return false;
 									}
-
-									return false;
 								}
-							}
 
-							return true;
-						});
+								return true;
+							});
 
-						return ParseResult { ChildTarget { hash(child_name), recursive }, parse_offset, false };
+							return ParseResult { ChildTarget { hash(child_name), recursive }, parse_offset, false };
+						}
+						else
+						{
+							return ParseResult { ChildTarget { hash(command_content) }, parse_offset, false };
+						}
 					}
-					else
-					{
-						return ParseResult { ChildTarget { hash(command_content) }, parse_offset, false };
-					}
-				}
+
+					break;
 
 				case "player"_hs:
-					if (auto player_index = util::from_string<PlayerIndex>(command_content))
+					if (!command_content.empty())
 					{
-						return ParseResult { PlayerTarget { *player_index }, parse_offset, false };
+						if (auto player_index = util::from_string<PlayerIndex>(command_content))
+						{
+							return ParseResult { PlayerTarget { *player_index }, parse_offset, false };
+						}
+						else if (auto as_indirect = command_content_as_indirect())
+						{
+							return *as_indirect;
+						}
 					}
 
 					break;
@@ -255,13 +377,99 @@ namespace engine
 
 				case "self"_hs:
 					return ParseResult { SelfTarget {}, parse_offset, false };
-			}
 
-			return ParseResult { EntityNameTarget { hash(command_content) }, parse_offset, false };
+				case "name"_hs:
+					if (!command_content.empty())
+					{
+						if (!is_string_content)
+						{
+							if (auto as_indirect = command_content_as_indirect())
+							{
+								return *as_indirect;
+							}
+						}
+					
+						return ParseResult { EntityNameTarget { hash(command_content) }, parse_offset, false };
+					}
+
+					break;
+			}
 		}
 
 		//return ParseResult { SelfTarget {}, 0, false };
 
 		return std::nullopt;
+	}
+
+	std::optional<EntityTarget> EntityTarget::parse(std::string_view raw_value, const MetaParsingInstructions* opt_parsing_instructions)
+	{
+		return from_parse_result(parse_type(raw_value, opt_parsing_instructions));
+	}
+
+	EntityTarget EntityTarget::from_parse_result(const ParseResult& result)
+	{
+		return EntityTarget { std::get<0>(result) };
+	}
+
+	std::optional<EntityTarget> EntityTarget::from_parse_result(const std::optional<ParseResult>& result)
+	{
+		if (!result)
+		{
+			return std::nullopt;
+		}
+
+		return from_parse_result(*result);
+	}
+
+	EntityTarget EntityTarget::from_entity(Entity entity)
+	{
+		return from_target_type(EntityTarget::ExactEntityTarget { entity });
+	}
+
+	EntityTarget EntityTarget::from_string(std::string_view raw_value)
+	{
+		if (auto result = parse(raw_value))
+		{
+			return std::move(*result);
+		}
+
+		return { EntityNameTarget { hash(raw_value) } };
+	}
+
+	EntityTarget EntityTarget::from_string(const std::string& raw_value)
+	{
+		return from_string(std::string_view { raw_value });
+	}
+
+	// EntityTarget::ChildTarget:
+	EntityTarget::ChildTarget EntityTarget::ChildTarget::from_string(std::string_view child_name, bool recursive)
+	{
+		return { hash(child_name).value(), recursive };
+	}
+
+	EntityTarget::ChildTarget EntityTarget::ChildTarget::from_string(std::string_view child_name)
+	{
+		return from_string(child_name, true);
+	}
+
+	EntityTarget::ChildTarget EntityTarget::ChildTarget::from_string(const std::string& child_name, bool recursive)
+	{
+		return from_string(std::string_view { child_name }, recursive);
+	}
+
+	EntityTarget::ChildTarget EntityTarget::ChildTarget::from_string(const std::string& child_name)
+	{
+		return from_string(child_name, true);
+	}
+
+	// EntityTarget::EntityNameTarget:
+	EntityTarget::EntityNameTarget EntityTarget::EntityNameTarget::from_string(std::string_view entity_name)
+	{
+		return { hash(entity_name).value() };
+	}
+
+	EntityTarget::EntityNameTarget EntityTarget::EntityNameTarget::from_string(const std::string& entity_name)
+	{
+		return from_string(std::string_view { entity_name });
 	}
 }
--- glare/src/engine/entity/entity_target.hpp
+++ glare/src/engine/entity/entity_target.hpp
@@ -2,6 +2,8 @@
 
 #include "types.hpp"
 
+#include <engine/meta/indirect_meta_any.hpp>
+
 #include <util/variant.hpp>
 
 #include <variant>
@@ -13,165 +15,203 @@
 
 namespace engine
 {
+	struct MetaEvaluationContext;
+	struct MetaParsingInstructions;
+
 	struct EntityTarget
 	{
-		using SelfTarget = std::monostate;
+		public:
+			using SelfTarget = std::monostate;
 
-		struct ParentTarget
-		{
-			inline bool operator==(const ParentTarget&) const noexcept { return true;  } // = default;
-			inline bool operator!=(const ParentTarget&) const noexcept { return false; } // = default;
-		};
-
-		struct ExactEntityTarget
-		{
-			Entity entity;
-
-			bool operator==(const ExactEntityTarget&) const noexcept = default;
-			bool operator!=(const ExactEntityTarget&) const noexcept = default;
-		};
-
-		struct ChildTarget
-		{
-			StringHash child_name;
-
-			bool recursive = true;
-
-			// TODO: Determine if it makes sense to ignore `recursive`. (Probably doesn't)
-			bool operator==(const ChildTarget&) const noexcept = default;
-			bool operator!=(const ChildTarget&) const noexcept = default;
-		};
-
-		struct EntityNameTarget
-		{
-			StringHash entity_name;
-
-			bool operator==(const EntityNameTarget&) const noexcept = default;
-			bool operator!=(const EntityNameTarget&) const noexcept = default;
-		};
-
-		struct PlayerTarget
-		{
-			PlayerIndex player_index;
-
-			bool operator==(const PlayerTarget&) const noexcept = default;
-			bool operator!=(const PlayerTarget&) const noexcept = default;
-		};
-
-		struct NullTarget
-		{
-			inline bool operator==(const NullTarget&) const noexcept { return true;  } // = default;
-			inline bool operator!=(const NullTarget&) const noexcept { return false; } // = default;
-		};
-
-		using TargetType = std::variant
-		<
-			SelfTarget, // std::monostate,
-			ParentTarget,
-			ExactEntityTarget,
-			EntityNameTarget,
-			ChildTarget,
-			PlayerTarget,
-			NullTarget
-		>;
-
-		enum TargetIndex : std::size_t
-		{
-			Self        = util::variant_index<TargetType, SelfTarget>(), // 0
-			Parent      = util::variant_index<TargetType, ParentTarget>(),
-			ExactEntity = util::variant_index<TargetType, ExactEntityTarget>(),
-			EntityName  = util::variant_index<TargetType, EntityNameTarget>(),
-			Child       = util::variant_index<TargetType, ChildTarget>(),
-			Player      = util::variant_index<TargetType, PlayerTarget>(),
-			Null        = util::variant_index<TargetType, NullTarget>(),
-		};
-
-		using Type = TargetType;
-
-		using ParseResult = std::tuple
-		<
-			TargetType,  // Type
-			std::size_t, // Parsing offset into `raw_value`.
-			bool         // `is_shorthand` (e.g. "self", "this", "parent")
-		>;
-
-		static std::optional<ParseResult> parse_type(std::string_view raw_value);
-		static std::optional<ParseResult> parse_type(const std::string& raw_value);
-
-		template <typename StringType>
-		static std::optional<EntityTarget> parse(StringType&& raw_value)
-		{
-			auto result = parse_type(std::forward<StringType>(raw_value));
-
-			if (!result)
+			struct ParentTarget
 			{
-				return std::nullopt;
+				inline bool operator==(const ParentTarget&) const noexcept { return true;  } // = default;
+				inline bool operator!=(const ParentTarget&) const noexcept { return false; } // = default;
+			};
+
+			struct ExactEntityTarget
+			{
+				Entity entity = null;
+
+				bool operator==(const ExactEntityTarget&) const noexcept = default;
+				bool operator!=(const ExactEntityTarget&) const noexcept = default;
+			};
+
+			struct ChildTarget
+			{
+				static ChildTarget from_string(std::string_view child_name, bool recursive);
+				static ChildTarget from_string(std::string_view child_name);
+
+				static ChildTarget from_string(const std::string& child_name, bool recursive);
+				static ChildTarget from_string(const std::string& child_name);
+
+				StringHash child_name = {};
+
+				bool recursive = true;
+
+				// TODO: Determine if it makes sense to ignore `recursive`. (Probably doesn't)
+				bool operator==(const ChildTarget&) const noexcept = default;
+				bool operator!=(const ChildTarget&) const noexcept = default;
+			};
+
+			struct EntityNameTarget
+			{
+				static EntityNameTarget from_string(std::string_view entity_name);
+				static EntityNameTarget from_string(const std::string& entity_name);
+
+				StringHash entity_name = {};
+
+				bool search_children_first = true;
+
+				bool operator==(const EntityNameTarget&) const noexcept = default;
+				bool operator!=(const EntityNameTarget&) const noexcept = default;
+			};
+
+			struct PlayerTarget
+			{
+				PlayerIndex player_index = NO_PLAYER;
+
+				bool operator==(const PlayerTarget&) const noexcept = default;
+				bool operator!=(const PlayerTarget&) const noexcept = default;
+			};
+
+			struct IndirectTarget
+			{
+				IndirectMetaAny target_value_expr;
+
+				bool operator==(const IndirectTarget&) const noexcept = default;
+				bool operator!=(const IndirectTarget&) const noexcept = default;
+			};
+
+			struct NullTarget
+			{
+				inline bool operator==(const NullTarget&) const noexcept { return true;  } // = default;
+				inline bool operator!=(const NullTarget&) const noexcept { return false; } // = default;
+			};
+
+			using TargetType = std::variant
+			<
+				SelfTarget, // std::monostate,
+				ParentTarget,
+				ExactEntityTarget,
+				EntityNameTarget,
+				ChildTarget,
+				PlayerTarget,
+				IndirectTarget,
+				NullTarget
+			>;
+
+			enum TargetIndex : std::size_t
+			{
+				Self        = util::variant_index<TargetType, SelfTarget>(), // 0
+				Parent      = util::variant_index<TargetType, ParentTarget>(),
+				ExactEntity = util::variant_index<TargetType, ExactEntityTarget>(),
+				EntityName  = util::variant_index<TargetType, EntityNameTarget>(),
+				Child       = util::variant_index<TargetType, ChildTarget>(),
+				Player      = util::variant_index<TargetType, PlayerTarget>(),
+				Indirect    = util::variant_index<TargetType, IndirectTarget>(),
+				Null        = util::variant_index<TargetType, NullTarget>(),
+			};
+
+			using Type = TargetType;
+
+			using ParseResult = std::tuple
+			<
+				TargetType,  // Type
+				std::size_t, // Parsing offset into `raw_value`.
+				bool         // `is_shorthand` (e.g. "self", "this", "parent")
+			>;
+
+			static std::optional<ParseResult> parse_type(std::string_view raw_value, const MetaParsingInstructions* opt_parsing_instructions={});
+
+			static std::optional<EntityTarget> parse(std::string_view raw_value, const MetaParsingInstructions* opt_parsing_instructions={});
+
+			static EntityTarget from_parse_result(const ParseResult& result);
+			static std::optional<EntityTarget> from_parse_result(const std::optional<ParseResult>& result);
+
+			template <typename TargetTypeExact>
+			static EntityTarget from_target_type(const TargetTypeExact& exact_target_type)
+			{
+				return EntityTarget { TargetType { exact_target_type } };
 			}
 
-			return EntityTarget { std::move(std::get<0>(*result)) };
-		}
+			static EntityTarget from_entity(Entity entity);
 
-		TargetType type = SelfTarget {};
+			static EntityTarget from_string(std::string_view raw_value);
+			static EntityTarget from_string(const std::string& raw_value);
 
-		inline operator TargetType() const
-		{
-			return type;
-		}
+			TargetType type = SelfTarget {};
 
-		// Attempts to resolve the targeted entity indicated by
-		// `type` from `source`, using `registry`.
-		// 
-		// NOTE: If `source` is `null`, any control path expecting to
-		// use or return the initial entity will fail.
-		Entity resolve(Registry& registry, Entity source=null) const;
+			inline operator TargetType() const
+			{
+				return type;
+			}
 
-		// Alias for `resolve`. (Useful for generic programming contexts)
-		inline Entity get(Registry& registry, Entity source=null) const
-		{
-			return resolve(registry, source);
-		}
+			// Alias for `get`.
+			Entity resolve(Registry& registry, Entity source=null) const;
 
-		bool operator==(const EntityTarget&) const noexcept = default;
-		bool operator!=(const EntityTarget&) const noexcept = default;
+			// Attempts to resolve the targeted entity indicated by
+			// `type` from `source`, using `registry`.
+			// 
+			// NOTE: If `source` is `null`, any control path expecting to
+			// use or return the initial entity will fail.
+			Entity get(Registry& registry, Entity source) const;
 
-		inline std::size_t target_index() const
-		{
-			return type.index();
-		}
+			// Attempts to resolve the targeted entity indicated by
+			// `type` from `source`, using `registry` and `context`.
+			Entity get(Registry& registry, Entity source, const MetaEvaluationContext& context) const;
 
-		inline bool is_self_targeted() const
-		{
-			return (target_index() == TargetIndex::Self); // 0 // std::monostate
-		}
+			bool operator==(const EntityTarget&) const noexcept = default;
+			bool operator!=(const EntityTarget&) const noexcept = default;
 
-		inline bool is_parent_target() const
-		{
-			return (target_index() == TargetIndex::Parent);
-		}
+			inline std::size_t target_index() const
+			{
+				return type.index();
+			}
 
-		inline bool is_exact_entity_target() const
-		{
-			return (target_index() == TargetIndex::ExactEntity);
-		}
+			inline bool is_self_targeted() const
+			{
+				return (target_index() == TargetIndex::Self); // 0 // std::monostate
+			}
 
-		inline bool is_entity_name_target() const
-		{
-			return (target_index() == TargetIndex::EntityName);
-		}
+			inline bool is_parent_target() const
+			{
+				return (target_index() == TargetIndex::Parent);
+			}
 
-		inline bool is_child_target() const
-		{
-			return (target_index() == TargetIndex::Child);
-		}
+			inline bool is_exact_entity_target() const
+			{
+				return (target_index() == TargetIndex::ExactEntity);
+			}
 
-		inline bool is_player_target() const
-		{
-			return (target_index() == TargetIndex::Player);
-		}
+			inline bool is_entity_name_target() const
+			{
+				return (target_index() == TargetIndex::EntityName);
+			}
 
-		inline bool is_null_target() const
-		{
-			return (target_index() == TargetIndex::Null);
-		}
+			inline bool is_child_target() const
+			{
+				return (target_index() == TargetIndex::Child);
+			}
+
+			inline bool is_player_target() const
+			{
+				return (target_index() == TargetIndex::Player);
+			}
+
+			inline bool is_indirect_target() const
+			{
+				return (target_index() == TargetIndex::Indirect);
+			}
+
+			inline bool is_null_target() const
+			{
+				return (target_index() == TargetIndex::Null);
+			}
+		private:
+			template <typename ...Args>
+			Entity get_impl(Registry& registry, Entity source, Args&&... args) const;
 	};
+
+	//static_assert(sizeof(EntityTarget) <= sizeof(double[2]));
 }
