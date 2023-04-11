abcd36732c713e8baaf7a11c702c7a500febf97b
Anthony Diamond <sonickid8@live.com>
2023-04-11 15:17:35 -0600

    Integrated `Service` and `SystemManagerInterface` into `MetaEvaluationContextStore`.

--- glare/src/engine/meta/meta_evaluation_context_store.cpp
+++ glare/src/engine/meta/meta_evaluation_context_store.cpp
@@ -4,23 +4,45 @@
 
 namespace engine
 {
-	MetaEvaluationContextStore::MetaEvaluationContextStore(MetaVariableEvaluationContext&& variable_context)
-		: variable_context(std::move(variable_context))
+	MetaEvaluationContextStore::MetaEvaluationContextStore
+	(
+		MetaVariableEvaluationContext&& variable_context,
+		Service* service,
+		SystemManagerInterface* system_manager
+	) :
+		variable_context(std::move(variable_context)),
+		service(service),
+		system_manager(system_manager)
 	{}
 
-	MetaEvaluationContextStore::MetaEvaluationContextStore(const MetaEvaluationContext& context)
-		: variable_context(*context.variable_context) {}
+	MetaEvaluationContextStore::MetaEvaluationContextStore(const MetaEvaluationContext& context) :
+		variable_context(*context.variable_context),
+		service(context.service),
+		system_manager(context.system_manager)
+	{}
 
 	MetaEvaluationContext MetaEvaluationContextStore::get_context() const
 	{
 		return MetaEvaluationContext
 		{
-			&variable_context
+			&variable_context,
+			service,
+			system_manager
 		};
 	}
 
 	const MetaVariableEvaluationContext& MetaEvaluationContextStore::get_variable_context() const
 	{
 		return variable_context;
+	}
+
+	const Service* MetaEvaluationContextStore::get_service() const // Service*
+	{
+		return service;
+	}
+
+	const SystemManagerInterface* MetaEvaluationContextStore::get_system_manager() const // SystemManagerInterface*
+	{
+		return system_manager;
 	}
 }
