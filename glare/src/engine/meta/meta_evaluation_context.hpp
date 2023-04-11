7b9b888b0a9850b1f8a15528da8a2e0021fc176f
Anthony Diamond <sonickid8@live.com>
2023-04-11 15:16:59 -0600

    Added `service` and `system_manager` fields to `MetaEvaluationContext`.

--- glare/src/engine/meta/meta_evaluation_context.hpp
+++ glare/src/engine/meta/meta_evaluation_context.hpp
@@ -7,9 +7,13 @@
 namespace engine
 {
 	class MetaVariableEvaluationContext;
+	class Service;
+	class SystemManagerInterface;
 
 	struct MetaEvaluationContext
 	{
 		MetaVariableEvaluationContext* variable_context = nullptr;
+		Service* service = nullptr;
+		SystemManagerInterface* system_manager = nullptr;
 	};
 }
