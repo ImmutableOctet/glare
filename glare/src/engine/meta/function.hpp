b62b9bea7dc860afa5c3bcbc701bfc9832a5c25b
Anthony Diamond <sonickid8@live.com>
2023-04-22 11:34:53 -0600

    Various tweaks to `invoke_any_overload`, added `attempt_default_argument_expansion` parameter, etc.

--- glare/src/engine/meta/function.hpp
+++ glare/src/engine/meta/function.hpp
@@ -40,11 +40,11 @@ namespace engine
 	}
 
 	template <typename InstanceType> // typename ...Args
-	MetaAny invoke_any_overload(MetaFunction function, InstanceType&& instance, MetaAny* const args, std::size_t arg_count)
+	MetaAny invoke_any_overload(MetaFunction function_overloads, InstanceType&& instance, MetaAny* const args, std::size_t arg_count, bool attempt_default_argument_expansion=false)
 	{
 		static_assert(std::is_same_v<std::decay_t<InstanceType>, MetaAny>, "The `instance` parameter must be a `MetaAny` object, or a reference to one.");
 
-		while (function)
+		for (auto function = function_overloads; (function); function = function.next())
 		{
 			// NOTE: We don't bother with the function `arity` check here,
 			// since EnTT already handles that. (+ Copying a pointer is essentially free)
@@ -55,8 +55,57 @@ namespace engine
 				    return result;
 			    }
             }
+		}
 
-			function = function.next();
+		if (attempt_default_argument_expansion)
+		{
+			using TemporaryArgumentStore = util::small_vector<MetaAny, 8>;
+
+			TemporaryArgumentStore forwarding_store;
+
+			forwarding_store.reserve(arg_count);
+
+			for (std::size_t provided_argument_index = 0; provided_argument_index < arg_count; provided_argument_index++)
+			{
+				forwarding_store.emplace_back(args[provided_argument_index].as_ref());
+			}
+
+			for (auto function = function_overloads; (function); function = function.next())
+			{
+				const auto intended_arg_count = function.arity();
+
+				if (arg_count < intended_arg_count)
+				{
+					bool attempt_defaulted_call = true;
+
+					for (std::size_t defaulted_argument_index = arg_count; defaulted_argument_index < intended_arg_count; defaulted_argument_index++)
+					{
+						auto arg_type = function.arg(defaulted_argument_index);
+
+						if (auto defaulted_argument = arg_type.construct())
+						{
+							forwarding_store.emplace_back(std::move(defaulted_argument));
+						}
+						else
+						{
+							attempt_defaulted_call = false;
+
+							break;
+						}
+					}
+
+					if (attempt_defaulted_call)
+					{
+						// NOTE: Const-cast needed due to limitations of EnTT's API.
+						if (auto result = function.invoke(instance, const_cast<MetaAny* const>(forwarding_store.data()), forwarding_store.size()))
+						{
+							return result;
+						}
+					}
+
+					forwarding_store.resize(arg_count);
+				}
+			}
 		}
 
 		return {};
@@ -66,11 +115,11 @@ namespace engine
     // If this fails, a second invocation pass will be attempted where any overload's parameters
     // of type `MetaAny` is handled via an `entt::forward_as_meta` passthrough.
     template <typename InstanceType>
-	static MetaAny invoke_any_overload_with_automatic_meta_forwarding(MetaFunction function, InstanceType&& instance, MetaAny* const args, std::size_t arg_count)
+	static MetaAny invoke_any_overload_with_automatic_meta_forwarding(MetaFunction function_overloads, InstanceType&& instance, MetaAny* const args, std::size_t arg_count)
 	{
 		static_assert(std::is_same_v<std::decay_t<InstanceType>, MetaAny>, "The `instance` parameter must be a `MetaAny` object, or a reference to one.");
 
-		if (auto result = invoke_any_overload(function, instance, args, arg_count))
+		if (auto result = invoke_any_overload(function_overloads, instance, args, arg_count))
 		{
 			return result;
 		}
@@ -79,7 +128,7 @@ namespace engine
 
 		TemporaryArgumentStore forwarding_store;
 
-		while (function)
+		for (auto function = function_overloads; (function); function = function.next())
 		{
 			const auto intended_arg_count = function.arity();
 
@@ -109,8 +158,6 @@ namespace engine
 
 				forwarding_store.clear();
 			}
-
-			function = function.next();
 		}
 
 		return {};
