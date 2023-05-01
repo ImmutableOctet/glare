4131e2f3f629f23b5dae660834b29b0eae14c8a0
Anthony Diamond <sonickid8@live.com>
2023-05-01 14:27:07 -0600

    Changed `MAGIC_ENUM_RANGE_MAX` to 2048.

--- glare/src/util/magic_enum.hpp
+++ glare/src/util/magic_enum.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #define MAGIC_ENUM_RANGE_MIN 0 // -128
-#define MAGIC_ENUM_RANGE_MAX 512 // 256 // 128
+#define MAGIC_ENUM_RANGE_MAX 2048 // 1024 // 512 // 32767 // 65535 // 256 // 128
 
 //#include <magic_enum/magic_enum.hpp>
 #include <magic_enum.hpp>
