b9ebd7ae595d00da261d6a1fa49cdbef554b71c4
Anthony Diamond <sonickid8@live.com>
2023-05-14 16:06:39 -0600

    Added `math::Tau`.

--- glare/src/math/conversion.hpp
+++ glare/src/math/conversion.hpp
@@ -12,7 +12,14 @@ namespace math
 		return T(3.141592653589793); // M_PI
 	}
 
+	template <typename T>
+	inline constexpr T _Tau()
+	{
+		return (static_cast<T>(2) * _Pi<T>());
+	}
+
 	constexpr auto Pi = _Pi<float>();
+	constexpr auto Tau = _Tau<float>();
 
 	template <typename T>
 	inline constexpr T degrees(T r)
