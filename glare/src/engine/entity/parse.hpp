cbc304a9ede49cce313721a13c9cf01bda113d6c
Anthony Diamond <sonickid8@live.com>
2023-05-30 12:01:38 -0600

    Added fields to the returned tuple of `parse_qualified_reference`. (Indicates whether the first or second symbol is using command syntax)
    Added `disallow_command_as_symbol` parameter to `parse_qualified_assignment_or_comparison`, allowing for immediate failure when command syntax is detected.

--- glare/src/engine/entity/parse.hpp
+++ glare/src/engine/entity/parse.hpp
@@ -21,10 +21,12 @@ namespace engine
 	std::tuple
 	<
 		std::optional<EntityTarget::ParseResult>, // entity_target_parse_result (optional)
-		std::string_view, // first_symbol (e.g. thread_name)
-		std::string_view, // second_symbol (e.g. variable_name)
-		std::string_view, // access_operator
-		std::size_t       // updated_offset
+		std::string_view,                         // first_symbol (e.g. thread_name)
+		std::string_view,                         // second_symbol (e.g. variable_name)
+		std::string_view,                         // access_operator
+		bool,                                     // first_symbol_is_command
+		bool,                                     // second_symbol_is_command
+		std::size_t                               // updated_offset
 	>
 	parse_qualified_reference
 	(
@@ -49,10 +51,13 @@ namespace engine
 		std::string_view condition_or_assignment,
 		std::size_t initial_offset=0,
 		std::string_view allowed_operators={},
+		
 		bool allow_missing_operator=false,
 		bool allow_empty_trailing_value=false,
 		bool allow_scope_as_implied_operator=true,
 		bool truncate_at_logical_operators=false,
+		bool disallow_command_as_symbol=true, // false,
+
 		const MetaParsingInstructions* opt_parsing_instructions={}
 	);
 }
