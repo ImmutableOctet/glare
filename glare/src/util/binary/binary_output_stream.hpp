18929642cf977b1adf9611ca2ce4fa0660ac853e
Anthony Diamond <sonickid8@live.com>
2023-07-10 13:55:01 -0600

    Implemented string support, added `can_write`, etc.

--- glare/src/util/binary/binary_output_stream.hpp
+++ glare/src/util/binary/binary_output_stream.hpp
@@ -6,6 +6,8 @@
 
 #include <utility>
 #include <type_traits>
+#include <string>
+#include <string_view>
 #include <stdexcept>
 
 namespace util
@@ -15,8 +17,10 @@ namespace util
 	class BinaryOutputStream
 	{
 		protected:
+			using StringLengthType = std::uint32_t; // std::uint16_t;
+
 			template <typename T, bool error_on_failure=true>
-			BinaryOutputStream& basic_write_from_impl(const T& value)
+			BinaryOutputStream& basic_write_from_trivial_impl(const T& value)
 			{
 				constexpr auto bytes_to_copy = sizeof(std::decay_t<T>);
 
@@ -48,6 +52,38 @@ namespace util
 				return *this;
 			}
 
+			template <typename StringType, bool error_on_failure=true>
+			BinaryOutputStream& basic_write_from_string_impl(const StringType& value)
+			{
+				const auto bytes_to_copy = value.size(); // length();
+				const auto length = static_cast<StringLengthType>(bytes_to_copy);
+
+				write(length);
+
+				if (!write_bytes(reinterpret_cast<const Byte*>(value.data()), bytes_to_copy))
+				{
+					if constexpr (error_on_failure)
+					{
+						throw std::runtime_error("Unable to complete write operation");
+					}
+				}
+
+				return *this;
+			}
+
+			template <typename T, bool error_on_failure=true>
+			BinaryOutputStream& basic_write_from_impl(const T& value)
+			{
+				if constexpr (std::is_same_v<std::decay_t<T>, std::string> || std::is_same_v<std::decay_t<T>, std::string_view>)
+				{
+					return basic_write_from_string_impl<T, error_on_failure>(value);
+				}
+				else
+				{
+					return basic_write_from_trivial_impl<T, error_on_failure>(value);
+				}
+			}
+
 			virtual bool write_bytes(const Byte* data_in, std::size_t count) = 0;
 
 			bool use_network_byte_order : 1 = false;
@@ -76,6 +112,22 @@ namespace util
 				return false;
 			}
 
+			inline virtual bool can_write(std::size_t count) const
+			{
+				return true;
+			}
+
+			inline virtual bool can_write() const
+			{
+				return can_write(static_cast<std::size_t>(1));
+			}
+
+			template <typename T>
+			bool can_write_value() const
+			{
+				return can_write(sizeof(T));
+			}
+
 			// Generalized implementation of `write_from`.
 			// (Calls `basic_read_to_impl` internally)
 			// 
@@ -93,6 +145,20 @@ namespace util
 				return basic_write_from_impl<T, error_on_failure>(value);
 			}
 
+			inline virtual BinaryOutputStream& write_from(const std::string& value)
+			{
+				constexpr bool error_on_failure = true;
+
+				return basic_write_from_impl<decltype(value), error_on_failure>(value);
+			}
+
+			inline virtual BinaryOutputStream& write_from(std::string_view value)
+			{
+				constexpr bool error_on_failure = true;
+
+				return basic_write_from_impl<decltype(value), error_on_failure>(value);
+			}
+
 			// Alias to `write_from`.
 			template <typename T>
 			BinaryOutputStream& write(T&& value)
@@ -107,8 +173,25 @@ namespace util
 				return write_from(value);
 			}
 
+			inline explicit operator bool() const
+			{
+				return can_write();
+			}
+
 			// This acts as shorthand for `set_output_position`.
 			inline bool seek_output(StreamPosition position)
+			{
+				return set_output_position(position);
+			}
+
+			// This acts as shorthand for `get_output_position`.
+			inline StreamPosition tellp() const
+			{
+				return get_output_position();
+			}
+
+			// This acts as shorthand for `set_output_position`.
+			inline bool seekp(StreamPosition position)
 			{
 				return set_output_position(position);
 			}
