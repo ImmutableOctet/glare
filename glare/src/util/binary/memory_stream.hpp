d149ea64699543cac41434bf731cc321ea3b3396
Anthony Diamond <sonickid8@live.com>
2023-07-12 15:38:07 -0600

    Added `use_network_byte_order` to `OwningMemoryStream`'s constructors.

--- glare/src/util/binary/memory_stream.hpp
+++ glare/src/util/binary/memory_stream.hpp
@@ -11,14 +11,14 @@ namespace util
 	class OwningMemoryStream : public FixedBinaryStream
 	{
 		public:
-			inline OwningMemoryStream(OwningBinaryDataBuffer&& buffer, bool sync_input_and_output=true)
-				: FixedBinaryStream(std::move(buffer), sync_input_and_output) {}
+			inline OwningMemoryStream(OwningBinaryDataBuffer&& buffer, bool sync_input_and_output=true, bool use_network_byte_order=false) :
+				FixedBinaryStream(std::move(buffer), sync_input_and_output, use_network_byte_order) {}
 
-			inline explicit OwningMemoryStream(std::size_t buffer_size, bool sync_input_and_output=true)
-				: OwningMemoryStream(OwningBinaryDataBuffer(buffer_size), sync_input_and_output) {}
+			inline explicit OwningMemoryStream(std::size_t buffer_size, bool sync_input_and_output=true, bool use_network_byte_order=false) :
+				OwningMemoryStream(OwningBinaryDataBuffer(buffer_size), sync_input_and_output, use_network_byte_order) {}
 
-			//inline OwningMemoryStream(bool sync_input_and_output=true)
-			//	: OwningMemoryStream(1024, sync_input_and_output) {}
+			//inline OwningMemoryStream(bool sync_input_and_output=true, bool use_network_byte_order=false)
+			//	: OwningMemoryStream(1024, sync_input_and_output, use_network_byte_order) {}
 
 			OwningMemoryStream(OwningMemoryStream&&) noexcept = default;
 			OwningMemoryStream& operator=(OwningMemoryStream&&) noexcept = default;
