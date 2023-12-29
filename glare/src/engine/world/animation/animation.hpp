2d4c16191585733a5d8bb2f213d060adfce62760
Anthony Diamond <sonickid8@live.com>
2023-12-29 17:36:01 -0500

    Removed legacy `animation` header.

--- glare/src/engine/world/animation/animation.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "animation_system.hpp"
-#include "animation_events.hpp"
-
-#include "components/animation_component.hpp"
-#include "components/bone_component.hpp"
-#include "components/skeletal_component.hpp"
