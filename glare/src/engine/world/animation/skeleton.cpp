79fbe42433bd3adcd8c8cc01f58eeef5a8aa7c92
Anthony Diamond <sonickid8@live.com>
2023-12-29 17:29:07 -0500

    Initial implementation of `Skeleton`.

---
+++ glare/src/engine/world/animation/skeleton.cpp
@@ -0,0 +1,167 @@
+#include "skeleton.hpp"
+
+#include <engine/meta/hash.hpp>
+
+#include <math/math.hpp>
+#include <util/string.hpp>
+
+#include <assimp/mesh.h>
+
+#include <cassert>
+
+namespace engine
+{
+	const Bone* Skeleton::get_bone_by_index(BoneIndex bone_index) const
+	{
+		assert(bone_index >= 0);
+
+		const auto bone_index_converted = static_cast<std::size_t>(bone_index);
+
+		if ((bone_index_converted < bones.size()))
+		{
+			return nullptr;
+		}
+
+		return &(bones[bone_index_converted]);
+	}
+
+	const Bone* Skeleton::get_bone_by_id(BoneID bone_id) const
+	{
+		for (const auto& bone : bones)
+		{
+			if (bone.name == bone_id)
+			{
+				return &bone;
+			}
+		}
+
+		return {};
+	}
+
+	const Bone* Skeleton::get_bone_by_name(std::string_view bone_name) const
+	{
+		if (bone_name.empty())
+		{
+			return {};
+		}
+
+		const auto bone_id = hash(bone_name);
+
+		return get_bone_by_id(bone_id);
+	}
+
+	const Bone* Skeleton::get_or_create_bone(std::string_view bone_name)
+	{
+		const auto bone_id = hash(bone_name);
+
+		return get_or_create_bone(bone_id);
+	}
+
+	const Bone* Skeleton::get_or_create_bone(BoneID bone_id)
+	{
+		return add_bone(bone_id);
+	}
+
+	const Bone* Skeleton::add_bone
+	(
+		BoneID bone_id,
+		const math::Matrix& node_transform,
+		const math::Matrix& offset,
+		BoneID parent_bone_id
+	)
+	{
+		if (const auto bone = get_bone_by_id(bone_id))
+		{
+			return bone;
+		}
+
+		const auto& bone_out = bones.emplace
+		(
+			bone_id,
+			parent_bone_id,
+			node_transform,
+			offset
+		);
+
+		return &bone_out;
+	}
+
+	const Bone* Skeleton::add_bone
+	(
+		BoneID bone_id,
+		BoneID parent_bone_id
+	)
+	{
+		return add_bone(bone_id, math::identity_matrix(), math::identity_matrix(), parent_bone_id);
+	}
+
+	std::optional<BoneIndex> Skeleton::get_index(const Bone* bone) const
+	{
+		if (!bone)
+		{
+			return std::nullopt;
+		}
+
+		const auto bones_begin = bones.data();
+		const auto bones_end   = (bones.data() + bones.size());
+
+		if ((bone < bones_begin) || (bone >= bones_end))
+		{
+			return std::nullopt;
+		}
+
+		return static_cast<BoneIndex>(bone - bones_begin);
+	}
+
+	std::optional<BoneIndex> Skeleton::get_index(std::string_view bone_name) const
+	{
+		if (const auto bone = get_bone_by_name(bone_name))
+		{
+			return get_index(bone);
+		}
+
+		return std::nullopt;
+	}
+
+	std::optional<BoneIndex> Skeleton::get_index(BoneID bone_id) const
+	{
+		if (const auto bone = get_bone_by_id(bone_id))
+		{
+			return get_index(bone);
+		}
+
+		return std::nullopt;
+	}
+
+	/*
+	const Bone* Skeleton::add_bone
+	(
+		const aiBone& bone_raw,
+		const math::Matrix& node_transform,
+
+		std::string_view parent_bone_name
+	)
+	{
+		return add_bone
+		(
+			hash(util::to_string_view(bone_raw.mName)),
+
+			node_transform,
+			
+			//math::to_matrix(bone_raw->mOffsetMatrix),
+			//(math::to_matrix(bone_raw->mOffsetMatrix) * glm::inverse(node_transform)),
+			//(node_transform * math::to_matrix(bone_raw->mOffsetMatrix)),
+			{},
+
+			hash(parent_bone_name)
+		);
+	}
+	*/
+
+	/*
+	const Bone* Skeleton::get_bone(const aiBone* bone_raw) const
+	{
+		return get_bone_by_name(util::to_string_view(bone_raw->mName));
+	}
+	*/
+}
---
+++ glare/src/engine/world/animation/skeleton.hpp
@@ -0,0 +1,102 @@
+#pragma once
+
+#include "types.hpp"
+#include "bone.hpp"
+
+#include <math/types.hpp>
+
+#include <string_view>
+#include <optional>
+#include <vector>
+
+//#include <util/small_vector.hpp>
+
+namespace engine
+{
+	// A primitive type used to store a static description of a skeleton.
+	// (Currently handled as a flat collection of `Bone`s; see `Container`)
+	// 
+	// NOTE: Parent/child relationships are handled by the `Bone` objects themselves.
+	struct Skeleton
+	{
+		// We use a vector-type here instead of a hash-map (or similar) due to the relatively small number of elements per-skeleton on average.
+		// TODO: Review pros/cons of switching to map container.
+		using Container = std::vector<Bone>; // util::small_vector<...>
+
+		// Attempts to retrieve a pointer to a `Bone` located at `bone_index`.
+		const Bone* get_bone_by_index(BoneIndex bone_index) const;
+
+		// Searches for a bone matching the `bone_id` specified.
+		const Bone* get_bone_by_id(BoneID bone_id) const;
+
+		// Searches for a bone whose identifier matches the hash of the `bone_name` specified.
+		const Bone* get_bone_by_name(std::string_view bone_name) const;
+
+		// Equivalent to `get_bone_by_name`.
+		inline const Bone* get_bone(std::string_view bone_name) const
+		{
+			return get_bone_by_name(bone_name);
+		}
+
+		const Bone* get_or_create_bone(std::string_view bone_name);
+		const Bone* get_or_create_bone(BoneID bone_id);
+
+		// Attempts to add a bone to this skeleton, returning a pointer to the internally stored object.
+		// 
+		// If the `bone_id` specified has already been added, this will
+		// return a pointer to an existing bone, rather than emplacing a new one.
+		const Bone* add_bone
+		(
+			BoneID bone_id,
+			const math::Matrix& node_transform,
+			const math::Matrix& offset,
+			BoneID parent_bone_id={}
+		);
+
+		// Attempts to add a bone to this skeleton, returning a pointer to the internally stored object.
+		// 
+		// If the `bone_id` specified has already been added, this will
+		// return a pointer to an existing bone, rather than emplacing a new one.
+		const Bone* add_bone
+		(
+			BoneID bone_id,
+			BoneID parent_bone_id={}
+		);
+
+		// Attempts to retrieve the index of the `bone` specified.
+		// If `bone` does not originate from this skeleton, this will return an empty optional.
+		std::optional<BoneIndex> get_index(const Bone* bone) const;
+
+		// Attempts to retrieve the index of the `bone_name` specified by computing a hash to act as an identifier.
+		std::optional<BoneIndex> get_index(std::string_view bone_name) const;
+
+		// Attempts to retrieve the index of the `bone_id` specified.
+		std::optional<BoneIndex> get_index(BoneID bone_id) const;
+
+		// Returns true if there are no bones in this skeleton.
+		inline bool empty() const
+		{
+			return bones.empty();
+		}
+
+		// Returns true if this skeleton contains at least one bone.
+		inline bool exists() const
+		{
+			return !empty();
+		}
+
+		inline std::size_t size() const
+		{
+			return bones.size();
+		}
+
+		// Equivalent to `exists`.
+		inline explicit operator bool() const
+		{
+			return exists();
+		}
+
+		// The bones owned by this skeleton.
+		Container bones;
+	};
+}
