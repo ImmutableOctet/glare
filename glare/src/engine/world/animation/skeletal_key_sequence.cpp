c2ab60ecc99395115745c2992bd9f7b05939a7a6
Anthony Diamond <sonickid8@live.com>
2024-01-28 15:18:25 -0500

    Reworked index handling in `SkeletalKeySequence`.

--- glare/src/engine/world/animation/skeletal_key_sequence.cpp
+++ glare/src/engine/world/animation/skeletal_key_sequence.cpp
@@ -6,33 +6,6 @@
 
 namespace engine
 {
-	template <typename KeyType, typename MixFn>
-	std::decay_t<KeyType> interpolate(const KeyType& from_frame, const KeyType& to_frame, float blend_factor, MixFn&& mix_fn)
-	{
-		return mix_fn(from_frame.value, to_frame.value, blend);
-	}
-
-	template <typename Keys, typename index_t, typename MixFn>
-	static auto interpolate(float timestamp, const Keys& keys, index_t index, MixFn&& mix_fn, bool flip_direction=false) -> decltype(Keys::value_type::value)
-	{
-		assert(!keys.empty());
-
-		if (keys.size() == 1)
-		{
-			return keys[0].value;
-		}
-
-		const auto& prev = keys[(index)];
-		const auto& next = keys[(index + 1)];
-
-		const auto blend = (flip_direction)
-			? SkeletalKeySequence::blend_factor(timestamp, next.timestamp, prev.timestamp)
-			: SkeletalKeySequence::blend_factor(timestamp, prev.timestamp, next.timestamp)
-		;
-
-		return interpolate();
-	}
-
 	float SkeletalKeySequence::linear_blend_factor(float timestamp, float prev_timestamp, float next_timestamp)
 	{
 		const auto elapsed_time = (timestamp - prev_timestamp);
@@ -41,12 +14,13 @@ namespace engine
 		return (elapsed_time / remaining_time);
 	}
 
-	math::Vector SkeletalKeySequence::interpolated_position(float timestamp) const
+	math::Vector SkeletalKeySequence::interpolated_position(float timestamp, float min_timestamp, float max_timestamp) const
 	{
 		return interpolate
 		(
-			timestamp, positions,
-			get_position_index(timestamp),
+			timestamp,
+			positions,
+			get_position_indices(timestamp, min_timestamp, max_timestamp),
 
 			[](const math::Vector& prev, const math::Vector& next, float blend)
 			{
@@ -55,12 +29,13 @@ namespace engine
 		);
 	}
 
-	math::Quaternion SkeletalKeySequence::interpolated_rotation(float timestamp, bool flip_direction) const
+	math::Quaternion SkeletalKeySequence::interpolated_rotation(float timestamp, float min_timestamp, float max_timestamp, bool flip_direction) const
 	{
 		return interpolate
 		(
-			timestamp, rotations,
-			get_rotation_index(timestamp),
+			timestamp,
+			rotations,
+			get_rotation_indices(timestamp, min_timestamp, max_timestamp),
 			
 			[](const math::Quaternion& prev, const math::Quaternion& next, float blend)
 			{
@@ -71,12 +46,13 @@ namespace engine
 		);
 	}
 
-	math::Vector SkeletalKeySequence::interpolated_scale(float timestamp) const
+	math::Vector SkeletalKeySequence::interpolated_scale(float timestamp, float min_timestamp, float max_timestamp) const
 	{
 		return interpolate
 		(
-			timestamp, scales,
-			get_scale_index(timestamp),
+			timestamp,
+			scales,
+			get_scale_indices(timestamp, min_timestamp, max_timestamp),
 			
 			[](const math::Vector& prev, const math::Vector& next, float blend)
 			{
@@ -85,26 +61,26 @@ namespace engine
 		);
 	}
 
-	math::Matrix SkeletalKeySequence::interpolated_position_matrix(float timestamp) const
+	math::Matrix SkeletalKeySequence::interpolated_position_matrix(float timestamp, float min_timestamp, float max_timestamp) const
 	{
-		return glm::translate(glm::mat4(1.0f), interpolated_position(timestamp));
+		return glm::translate(glm::mat4(1.0f), interpolated_position(timestamp, min_timestamp, max_timestamp));
 	}
 
-	math::Matrix SkeletalKeySequence::interpolated_rotation_matrix(float timestamp, bool flip_direction) const
+	math::Matrix SkeletalKeySequence::interpolated_rotation_matrix(float timestamp, float min_timestamp, float max_timestamp, bool flip_direction) const
 	{
-		return glm::toMat4(interpolated_rotation(timestamp, flip_direction));
+		return glm::toMat4(interpolated_rotation(timestamp, min_timestamp, max_timestamp, flip_direction));
 	}
 
-	math::Matrix SkeletalKeySequence::interpolated_scale_matrix(float timestamp) const
+	math::Matrix SkeletalKeySequence::interpolated_scale_matrix(float timestamp, float min_timestamp, float max_timestamp) const
 	{
-		return glm::scale(glm::mat4(1.0f), interpolated_scale(timestamp));
+		return glm::scale(glm::mat4(1.0f), interpolated_scale(timestamp, min_timestamp, max_timestamp));
 	}
 
-	math::Matrix SkeletalKeySequence::interpolated_matrix(float timestamp, bool flip_rotation) const
+	math::Matrix SkeletalKeySequence::interpolated_matrix(float timestamp, float min_timestamp, float max_timestamp, bool flip_rotation) const
 	{
-		const auto translation = interpolated_position_matrix(timestamp);
-		const auto rotation    = interpolated_rotation_matrix(timestamp, flip_rotation);
-		const auto scale       = interpolated_scale_matrix(timestamp);
+		const auto translation = interpolated_position_matrix(timestamp, min_timestamp, max_timestamp);
+		const auto rotation    = interpolated_rotation_matrix(timestamp, min_timestamp, max_timestamp, flip_rotation);
+		const auto scale       = interpolated_scale_matrix(timestamp, min_timestamp, max_timestamp);
 
 		return (translation * rotation * scale);
 	}
--- glare/src/engine/world/animation/skeletal_key_sequence.hpp
+++ glare/src/engine/world/animation/skeletal_key_sequence.hpp
@@ -1,109 +1,139 @@
 #pragma once
 
+#include "types.hpp"
+
 #include <math/types.hpp>
 
 #include <optional>
+#include <utility>
 #include <vector>
 
 #include <cstdint>
+#include <cassert>
 
 namespace engine
 {
 	struct SkeletalKeySequence
 	{
-		using index_t = std::uint32_t; // int;
+		using index_t = std::uint32_t;
 
-		struct KeyVector
+		template <typename T, typename TimestampType=float>
+		struct KeyData
 		{
-			/*
-			union
+			using value_type = T;
+			using timestamp_type = TimestampType;
+
+			value_type value;
+
+			timestamp_type timestamp;
+
+			explicit operator value_type() const
 			{
-				// Can be accessed as a position or scale.
-				math::Vector position;
-				math::Vector scale;
-				math::Vector value;
-			};
+				return value;
+			}
 
-			KeyVector() = default;
-			KeyVector(const KeyVector& v) = default
-			KeyVector(KeyVector&&) noexcept = default;
-
-			inline KeyVector(const math::Vector& vec, float timestamp)
-				: value(vec), timestamp(timestamp) {}
-			*/
-
-			math::Vector value;
-
-			float timestamp;
+			explicit operator const value_type&() const
+			{
+				return value;
+			}
 		};
 
-		struct KeyRotation
-		{
-			/*
-			union
-			{
-				math::Quaternion orientation;
-				math::Quaternion rotation;
-				math::Quaternion value;
-			};
-
-			KeyRotation() = default;
-			KeyRotation(const KeyRotation&) = default;
-			KeyRotation(KeyRotation&&) noexcept = default;
-
-			inline KeyRotation(const math::Quaternion& quat, float timestamp)
-				: value(quat), timestamp(timestamp) {}
-			*/
-
-			math::Quaternion value;
-
-			float timestamp;
-		};
+		using KeyVector   = KeyData<math::Vector>;
+		using KeyRotation = KeyData<math::Quaternion>;
 
 		using KeyPosition = KeyVector;
 		using KeyScale    = KeyVector;
 
-		// Retrieves the index of an element of `data` whose `timestamp` value is greater than or equal to `timestamp`.
+		// Retrieves the index of an element of `data` whose `timestamp` value is greater than or equal to `timestamp`,
+		// and within the range described by `min_timestamp` and `max_timestamp`.
 		template <typename Collection>
-		static std::optional<index_t> get_index(const Collection& data, float timestamp)
+		static std::pair<std::optional<index_t>, std::optional<index_t>> get_indices(const Collection& data, float timestamp, float min_timestamp, float max_timestamp)
 		{
-			if (data.empty())
+			if (!data.empty())
 			{
-				return std::nullopt;
-			}
-
-			for (std::size_t index = 0; index < (data.size() - 1); ++index)
-			{
-				if (timestamp < (data[static_cast<std::size_t>(index + 1)].timestamp))
+				for (index_t index = {}; index < static_cast<index_t>(data.size() - 1); ++index)
 				{
-					return static_cast<index_t>(index);
+					const auto& entry = data[index];
+
+					if ((entry.timestamp >= min_timestamp) && (entry.timestamp <= max_timestamp))
+					{
+						if (entry.timestamp >= timestamp)
+						{
+							const auto next_index = (index + 1);
+							const auto& next_entry = data[next_index];
+
+							if ((next_entry.timestamp >= min_timestamp) && (next_entry.timestamp <= max_timestamp))
+							{
+								return { index, next_index };
+							}
+
+							return { index, std::nullopt };
+						}
+					}
 				}
 			}
 
-			if (data.size() == 1)
+			return { std::nullopt, std::nullopt };
+		}
+
+		template <typename KeyType, typename MixFn>
+		static auto interpolate(const KeyType& from_frame, const KeyType& to_frame, float blend_factor, MixFn&& mix_fn)
+		{
+			return mix_fn(from_frame.value, to_frame.value, blend_factor);
+		}
+
+		template <typename Keys, typename index_t, typename MixFn>
+		static auto interpolate
+		(
+			float timestamp,
+			
+			const Keys& keys,
+			
+			std::pair<std::optional<index_t>, std::optional<index_t>> indices,
+
+			MixFn&& mix_fn,
+			
+			bool flip_direction=false
+		) -> decltype(Keys::value_type::value)
+		{
+			const auto& [from_index, to_index] = indices;
+
+			assert(from_index);
+			assert(static_cast<std::size_t>(*from_index) < keys.size());
+
+			const auto& prev = keys[*from_index];
+
+			if (!to_index)
 			{
-				return 0;
+				return prev.value;
 			}
 
-			return std::nullopt;
+			const auto& next = keys[*to_index];
+
+			const auto blend_factor = (flip_direction)
+				? SkeletalKeySequence::linear_blend_factor(timestamp, next.timestamp, prev.timestamp)
+				: SkeletalKeySequence::linear_blend_factor(timestamp, prev.timestamp, next.timestamp)
+			;
+
+			return interpolate(prev, next, blend_factor, std::forward<MixFn>(mix_fn));
 		}
 
 		// Computes a linear blend factor for `timestamp` using the area between `prev_timestamp` and `next_timestamp`.
 		static float linear_blend_factor(float timestamp, float prev_timestamp, float next_timestamp);
 
-		inline std::optional<index_t> get_position_index(float timestamp) const { return get_index(positions, timestamp); }
-		inline std::optional<index_t> get_rotation_index(float timestamp) const { return get_index(rotations, timestamp); }
-		inline std::optional<index_t> get_scale_index(float timestamp)    const { return get_index(scales, timestamp);    }
+		inline auto get_position_indices(float timestamp, float min_timestamp, float max_timestamp) const { return get_indices(positions, timestamp, min_timestamp, max_timestamp); }
+		inline auto get_rotation_indices(float timestamp, float min_timestamp, float max_timestamp) const { return get_indices(rotations, timestamp, min_timestamp, max_timestamp); }
+		inline auto get_scale_indices(float timestamp, float min_timestamp, float max_timestamp)    const { return get_indices(scales, timestamp, min_timestamp, max_timestamp);    }
 
-		math::Vector interpolated_position(float timestamp) const;
-		math::Quaternion interpolated_rotation(float timestamp, bool flip_direction=false) const;
-		math::Vector interpolated_scale(float timestamp) const;
+		math::Vector interpolated_position(float timestamp, float min_timestamp, float max_timestamp) const;
+		math::Quaternion interpolated_rotation(float timestamp, float min_timestamp, float max_timestamp, bool flip_direction=false) const;
+		math::Vector interpolated_scale(float timestamp, float min_timestamp, float max_timestamp) const;
 
-		math::Matrix interpolated_position_matrix(float timestamp) const;
-		math::Matrix interpolated_rotation_matrix(float timestamp, bool flip_direction=false) const;
-		math::Matrix interpolated_scale_matrix(float timestamp) const;
+		math::Matrix interpolated_position_matrix(float timestamp, float min_timestamp, float max_timestamp) const;
+		math::Matrix interpolated_rotation_matrix(float timestamp, float min_timestamp, float max_timestamp, bool flip_direction=false) const;
+		math::Matrix interpolated_scale_matrix(float timestamp, float min_timestamp, float max_timestamp) const;
 
-		math::Matrix interpolated_matrix(float timestamp, bool flip_rotation=false) const;
+		math::Matrix interpolated_matrix(float timestamp, float min_timestamp, float max_timestamp, bool flip_rotation=false) const;
 
 		std::vector<KeyPosition> positions;
 		std::vector<KeyRotation> rotations;
