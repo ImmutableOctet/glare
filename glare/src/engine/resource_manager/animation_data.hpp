584752a5b17c0648a58929ffdc9bf5d544d4716e
Anthony Diamond <sonickid8@live.com>
2024-01-28 15:10:17 -0500

    Changed `animations` to use `AnimationContainer` (slices), rather than a repository.

--- glare/src/engine/resource_manager/animation_data.hpp
+++ glare/src/engine/resource_manager/animation_data.hpp
@@ -1,18 +1,25 @@
 #pragma once
 
 #include <engine/types.hpp>
-#include <graphics/types.hpp>
 
+#include <engine/world/animation/types.hpp>
+#include <engine/world/animation/animation_slice.hpp>
 #include <engine/world/animation/skeletal_frame_data.hpp>
 #include <engine/world/animation/skeleton.hpp>
-#include <engine/world/animation/animation_repository.hpp>
+
+//#include <engine/world/animation/animation_repository.hpp>
+
+#include <unordered_map>
 
 namespace engine
 {
 	struct AnimationData
 	{
+		using SliceContainer = std::unordered_map<AnimationID, AnimationSlice>; // AnimationRepository::SliceContainer;
+		using AnimationContainer = SliceContainer;
+
 		Skeleton skeleton;
 		SkeletalFrameData frames;
-		AnimationRepository animations;
+		AnimationContainer animations;
 	};
 }
