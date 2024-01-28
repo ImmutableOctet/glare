e4ae02ee7343bd5dbb76f5706f165fb9ecef0986
Anthony Diamond <sonickid8@live.com>
2024-01-28 15:04:50 -0500

    Added `REFLECT_MEMBER_FUNCTION_OVERLOADS` macro for simplified usage of `engine::make_overloads`.

--- glare/src/engine/reflection/function.hpp
+++ glare/src/engine/reflection/function.hpp
@@ -9,6 +9,26 @@
 #include <type_traits>
 #include <cstddef>
 
+// Convenience macro for `engine::make_overloads`.
+#define REFLECT_MEMBER_FUNCTION_OVERLOADS(type_var, ClassName, FunctionName, MinArgCount, ReturnType, ...) \
+	type_var = engine::make_overloads                                                                  \
+	<                                                                                                  \
+		static_cast<ReturnType(ClassName::*)(__VA_ARGS__)>(&ClassName::FunctionName),                  \
+		[](auto& self, auto&&... args)                                                                 \
+		{                                                                                              \
+			if constexpr (std::is_same_v<std::decay_t<ReturnType>, void>)                              \
+			{                                                                                          \
+				self.FunctionName(std::forward<decltype(args)>(args)...);                              \
+			}                                                                                          \
+			else                                                                                       \
+			{                                                                                          \
+				                                                                                       \
+				return self.FunctionName(std::forward<decltype(args)>(args)...);                       \
+			}                                                                                          \
+		},                                                                                             \
+		MinArgCount                                                                                    \
+	> (type_var, engine::hash(#FunctionName));
+
 namespace engine
 {
 	// Generates a wrapper function for `lambda_instance`, allowing a captureless
