5a5c44159365bd28753554a8eec4351074b37d6f
Anthony Diamond <sonickid8@live.com>
2024-01-28 15:36:44 -0500

    Cleaned up `BoneComponent` and related API.

--- glare/src/engine/world/animation/components/bone_component.cpp
+++ glare/src/engine/world/animation/components/bone_component.cpp
@@ -1,33 +1,55 @@
 #include "bone_component.hpp"
 #include "skeletal_component.hpp"
 
+#include <engine/meta/hash.hpp>
+
+#include <engine/world/animation/bone.hpp>
+#include <engine/world/animation/skeleton.hpp>
+
 #include <engine/components/relationship_component.hpp>
 #include <engine/components/name_component.hpp>
 #include <engine/components/forwarding_component.hpp>
 
 #include <engine/world/world.hpp>
 
+#include <string>
+
 // Debugging related:
 #include <engine/world/graphics_entity.hpp>
 
 namespace engine
 {
-	std::tuple<bool, Entity> build_ligaments(World& world, Entity parent, const graphics::Skeleton& skeleton, const std::pair<std::string, graphics::Bone>& bone_entry, bool generate_parent, bool check_for_existing)
+	std::tuple<bool, Entity> build_ligaments
+	(
+		World& world,
+		Entity parent,
+		
+		const Skeleton& skeleton,
+		const Bone& bone,
+
+		bool generate_parent,
+		bool check_for_existing
+	)
 	{
-		const auto& bone_name = bone_entry.first;
-		const auto& bone_data = bone_entry.second;
-
-		const auto& parent_name = bone_data.parent_name;
-
 		if (generate_parent)
 		{
-			if (!parent_name.empty())
+			if (const auto& parent_id = bone.parent_name)
 			{
-				auto parent_it = skeleton.bones.find(parent_name);
-
-				if (parent_it != skeleton.bones.end())
+				if (auto parent_bone = skeleton.get_bone_by_id(parent_id))
 				{
-					auto new_parent = std::get<1>(build_ligaments(world, parent, skeleton, *parent_it, true, true));
+					// NOTE: Recursion.
+					auto new_parent = std::get<1>
+					(
+						build_ligaments
+						(
+							world, parent,
+							
+							skeleton,
+							*parent_bone,
+
+							true, true
+						)
+					);
 
 					if (new_parent != null)
 					{
@@ -37,9 +59,13 @@ namespace engine
 			}
 		}
 
+		const auto bone_index = skeleton.get_bone_index(&bone);
+
+		assert(bone_index);
+
 		if (check_for_existing)
 		{
-			auto existing = world.get_bone_by_name(parent, bone_name, true); // false
+			auto existing = world.get_bone_by_index(parent, *bone_index, true);
 
 			if (existing != null)
 			{
@@ -47,29 +73,42 @@ namespace engine
 			}
 		}
 
-		return { true, create_bone(world, bone_data, bone_name, parent) };
+		const auto bone_name = get_known_string_from_hash(bone.name);
+
+		return { true, create_bone(world, *bone_index, bone, bone_name, parent) };
 	}
 
-	Entity create_bone(World& world, BoneID bone_id, const math::Matrix& local_transform, const math::Matrix& offset, std::string_view bone_name, Entity parent, EntityType type)
+	Entity create_bone
+	(
+		World& world,
+		
+		BoneIndex bone_index,
+		const Bone& bone,
+		
+		std::string_view bone_name,
+		
+		Entity parent,
+		EntityType type
+	)
 	{
 		auto& registry = world.get_registry();
 
 		auto entity = engine::create_pivot(world, parent, type);
+
+		// Debugging related:
 		//auto entity = engine::load_model(world, "assets/geometry/cone.b3d", parent, type, false);
 
-		// TODO: Look into optimizing out BoneComponent's `name` field.
-		registry.emplace<NameComponent>(entity, std::string(bone_name));
-
-		BoneComponent* parent_bone_component = nullptr;
+		if (!bone_name.empty())
+		{
+			registry.emplace<NameComponent>(entity, std::string { bone_name });
+		}
 
 		Entity skeleton = null;
 
 		if (parent != null)
 		{
 			// Check if the parent entity is also a bone:
-			parent_bone_component = registry.try_get<BoneComponent>(parent);
-
-			if (parent_bone_component)
+			if (auto parent_bone_component = registry.try_get<BoneComponent>(parent))
 			{
 				// Forward the `skeleton` field from the parent entity.
 				// (Points to top-most parent in chain; see below)
@@ -79,83 +118,26 @@ namespace engine
 			{
 				// This parent entity isn't a bone, attach a skeleton to it.
 				// NOTE: The root bone will be set to `entity` if no skeleton existed previously.
-				attach_skeleton(world, parent, entity);
+				attach_skeleton(world.get_registry(), parent, entity);
 
 				// Set our skeleton instance to `parent` regardless of root/nested status.
 				skeleton = parent;
 			}
 		}
 
-		registry.emplace<BoneComponent>(entity, skeleton, bone_id, std::string(bone_name), offset); // offset;
+		registry.emplace<BoneComponent>(entity, skeleton, bone_index);
 
 		// Forward relevant events, logic, etc. to the `skeleton` entity.
 		registry.emplace<ForwardingComponent>(entity, skeleton);
 
 		{
+			const auto& local_transform = bone.node_transform;
+
 			auto tform = world.get_transform(entity);
 
-			tform.set_local_matrix(local_transform); // set_matrix(...)
+			tform.set_local_matrix(local_transform);
 		}
-
-		// Debugging related:
-		/*
-		auto dbg_parent = entity; // null;
-
-		auto dbg = engine::load_model(world, "assets/geometry/directions.b3d", dbg_parent, type, false);
-		//auto dbg = engine::load_model(world, "assets/geometry/cube.b3d", dbg_parent, type, false);
-
-		auto* dbg_name = registry.try_get<NameComponent>(dbg);
-
-		if (dbg_name)
-		{
-			dbg_name->set_name("Debug"); // "Debug Orientation";
-		}
-		*/
-
-		//auto dbg = engine::load_model(world, "assets/geometry/sphere.b3d", dbg_parent, type, false);
-		//auto dbg = engine::load_model(world, "assets/geometry/cone.b3d", dbg_parent, type, false);
-
-		//auto dbg = entity;
-
-		/*
-		{
-			auto dbg_tform = world.get_transform(dbg);
-
-			dbg_tform.set_local_matrix(offset);
-			//dbg_tform.set_local_matrix(local_transform);
-		}
-		*/
-
-		// Debugging related:
-		/*
-		auto* model_comp = registry.try_get<ModelComponent>(dbg);
-
-		if (model_comp)
-		{
-			if (bone_id == 1)
-			{
-				model_comp->color = graphics::ColorRGBA{ 1.0f, 1.0f, 1.0f, 1.0f };
-			}
-			else
-			{
-				switch ((bone_id % 3) + 1)
-				{
-					case 1:
-						model_comp->color = graphics::ColorRGBA{ 0.0f, 0.0f, 1.0f, 1.0f }; break;
-					case 2:
-						model_comp->color = graphics::ColorRGBA{ 0.0f, 1.0f, 0.0f, 1.0f }; break;
-					case 3:
-						model_comp->color = graphics::ColorRGBA{ 1.0f, 0.0f, 0.0f, 1.0f }; break;
-				}
-			}
-		}
-		*/
 
 		return entity;
-	}
-
-	Entity create_bone(World& world, const graphics::Bone& bone_data, std::string_view bone_name, Entity parent, EntityType type)
-	{
-		return create_bone(world, bone_data.id, bone_data.node_transform, bone_data.offset, bone_name, parent, type);
 	}
 }
--- glare/src/engine/world/animation/components/bone_component.hpp
+++ glare/src/engine/world/animation/components/bone_component.hpp
@@ -1,30 +1,25 @@
 #pragma once
 
-#include <math/types.hpp>
 #include <engine/types.hpp>
 
-// TODO: Look into workarounds for including this header directly.
-#include <graphics/skeleton.hpp>
+#include <math/types.hpp>
 
-#include <string>
-
-namespace graphics
-{
-	struct Bone;
-	struct Skeleton;
-}
+#include <string_view>
 
 namespace engine
 {
+	struct Bone;
+	struct Skeleton;
+
 	// Bones are special entities used for animation.
 	// `BoneComponent` is currently only valid to be attached to entities on creation.
-	// To create a bone, use the `create_bone`, `build_ligaments` or similar.
+	// To create a bone, use `create_bone`, `build_ligaments` or similar.
 	struct BoneComponent
 	{
 		/*
-			The skeleton this bone belongs to.
+			The root entity, owner of the skeleton this bone belongs to.
 			
-			If this bone's parent (via `RelationshipComponent` component) is changed, this will automatically
+			If this bone's parent (via `RelationshipComponent`) is changed, this will automatically
 			reflect the new skeleton of that entity. Likewise, if this entity's parent is changed to a
 			bone from a different skeleton, this bone will adopt that bone's `skeleton` field.
 			
@@ -32,14 +27,32 @@ namespace engine
 		*/
 		Entity skeleton;
 
-		BoneID ID;
-		std::string name;
-
-		math::Matrix offset;
+		// The index of the bone that this entity represents.
+		BoneIndex bone_index;
 	};
 
-	std::tuple<bool, Entity> build_ligaments(World& world, Entity parent, const graphics::Skeleton& skeleton, const std::pair<std::string, graphics::Bone>& bone_entry, bool generate_parent=true, bool check_for_existing=true);
+	std::tuple<bool, Entity> build_ligaments
+	(
+		World& world,
+		Entity parent,
+		
+		const Skeleton& skeleton,
+		const Bone& bone,
 
-	Entity create_bone(World& world, BoneID bone_id, const math::Matrix& local_transform, const math::Matrix& offset, std::string_view bone_name={}, Entity parent=null, EntityType type=EntityType::Bone);
-	Entity create_bone(World& world, const graphics::Bone& bone_data, std::string_view bone_name = {}, Entity parent=null, EntityType type=EntityType::Bone);
+		bool generate_parent=true,
+		bool check_for_existing=true
+	);
+
+	Entity create_bone
+	(
+		World& world,
+
+		BoneIndex bone_index,
+		const Bone& bone,
+
+		std::string_view bone_name={},
+		
+		Entity parent=null,
+		EntityType type=EntityType::Bone
+	);
 }
