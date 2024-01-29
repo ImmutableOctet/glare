183f49a577c54cf0c83c568a07f72d08f98b9cd8
Anthony Diamond <sonickid8@live.com>
2024-01-29 16:24:24 -0500

    Implemented initial support for recursive reflection using `reflect_aggregate_fields`. (Experimental)

--- glare/src/engine/reflection/pfr_reflection.hpp
+++ glare/src/engine/reflection/pfr_reflection.hpp
@@ -1,53 +1,68 @@
 #pragma once
 
-#include <engine/meta/hash.hpp>
+#include <engine/meta/types.hpp>
 
 #include <util/pfr.hpp>
 #include <util/type_algorithms.hpp>
 
+#include <entt/meta/meta.hpp>
+
 #include <type_traits>
+#include <string_view>
 #include <utility>
 
 namespace engine
 {
-	template <typename T>
-	auto reflect_aggregate_fields(auto type)
+	//=[]<typename NewType>(){ return engine_meta_type<NewType>(); }
+	template <typename T, bool recursive=true>
+	decltype(auto) reflect_aggregate_fields(auto&& type, auto&& new_type_fn)
 	{
-		constexpr auto field_count = util::pfr::tuple_size_v<std::remove_reference_t<T>>;
+		// NOTE: Recursion handled at this level, rather than via `enumerate_aggregate_fields`.
+		util::enumerate_aggregate_fields<T, false>
+		(
+			[&type, &new_type_fn]<typename SelfType, typename MemberType, auto total_member_count, auto member_index, auto member_name, auto member_id>()
+			{
+				static_assert(!std::is_same_v<std::remove_reference_t<SelfType>, std::remove_reference_t<MemberType>>);
 
-		if constexpr (field_count)
-		{
-			using Test = std::remove_reference_t<T>;
-
-			constexpr auto field_names = util::pfr::names_as_array<Test>();
-
-			/*
-			util::static_iota<field_count>
-			(
-				[&field_names, &type]<auto field_index>()
+				if constexpr (recursive)
 				{
-					const auto& field_name = field_names[field_index];
-					const auto field_name_id = engine::hash(std::string_view { field_name.data(), field_name.size() }).value();
+					const auto nested_type_already_reflected = engine::resolve<MemberType>();
 
-					using member_type = std::remove_reference_t<decltype(util::pfr::get<field_index, T>(std::declval<T>()))>;
-
-					constexpr auto getter_ptr = (+[](const T& instance) { return util::pfr::get<field_index>(instance); });
-
-					if constexpr (std::is_copy_assignable_v<member_type>)
+					if (!nested_type_already_reflected)
 					{
-						constexpr auto setter_ptr = (+[](T& instance, const member_type& member_value) { util::pfr::get<field_index>(instance) = member_value; });
-
-						type = type.data<setter_ptr, getter_ptr>(field_name_id);
-					}
-					else
-					{
-						type = type.data<nullptr, getter_ptr>(field_name_id);
+						reflect_aggregate_fields<T, recursive>(new_type_fn);
 					}
 				}
-			);
-			*/
-		}
+				
+				constexpr auto getter_ptr = (+[](const SelfType& instance) { return util::pfr::get<member_index>(instance); });
+
+				if constexpr (std::is_copy_assignable_v<MemberType>)
+				{
+					constexpr auto setter_ptr = (+[](SelfType& instance, const MemberType& member_value) { util::pfr::get<member_index>(instance) = member_value; });
+
+					type = type.data<setter_ptr, getter_ptr>(member_id);
+				}
+				else if constexpr (std::is_move_assignable_v<MemberType>)
+				{
+					constexpr auto setter_ptr = (+[](SelfType& instance, MemberType&& member_value) { util::pfr::get<member_index>(instance) = std::move(member_value); });
+
+					type = type.data<setter_ptr, getter_ptr>(member_id);
+				}
+				else
+				{
+					type = type.data<nullptr, getter_ptr>(member_id);
+				}
+			}
+		);
 
 		return type;
+	}
+
+	template <typename T, bool recursive=true>
+	decltype(auto) reflect_aggregate_fields(auto&& new_type_fn)
+	{
+		auto type = new_type_fn();
+
+		return reflect_aggregate_fields<T, recursive>(std::forward<decltype(type)>(type), std::forward<decltype(new_type_fn)>(new_type_fn));
 	}
 }
