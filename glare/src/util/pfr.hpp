752ac4e70dc99875f0ee4c3574711030e5f3ed44
Anthony Diamond <sonickid8@live.com>
2024-01-31 02:32:32 -0500

    Added static storage duration to constexpr variables in `enumerate_aggregate_fields_ex`, changed passing of member names to const-reference, etc.

--- glare/src/util/pfr.hpp
+++ glare/src/util/pfr.hpp
@@ -20,20 +20,19 @@ namespace util
 	template <typename T, bool recursive=false, typename MemberCallback>
 	constexpr void enumerate_aggregate_fields_ex(MemberCallback&& member_fn)
 	{
-		constexpr auto field_count = pfr::tuple_size_v<std::remove_reference_t<T>>;
+		static constexpr auto field_count = pfr::tuple_size_v<std::remove_reference_t<T>>;
 
 		if constexpr (field_count)
 		{
 			using Test = std::remove_reference_t<T>;
 
-			constexpr auto field_names = pfr::names_as_array<Test>();
+			static constexpr auto field_names = pfr::names_as_array<Test>();
 
 			static_iota<field_count>
 			(
-				[&member_fn, &field_names, field_count, recursive]<auto field_index>()
+				[&]<auto field_index>()
 				{
-					constexpr auto& field_name_raw = field_names[field_index];
-					constexpr auto field_name = std::string_view { field_name_raw.data(), field_name_raw.size() };
+					static constexpr auto& field_name = field_names[field_index];
 
 					using self_type = T;
 					using member_type = std::remove_reference_t<decltype(pfr::get<field_index, T>(std::declval<T>()))>;
@@ -54,7 +53,7 @@ namespace util
 	{
 		enumerate_aggregate_fields_ex<T, recursive>
 		(
-			[&member_fn]<typename SelfType, typename MemberType, auto total_member_count, auto member_index, auto member_name>()
+			[&member_fn]<typename SelfType, typename MemberType, auto total_member_count, auto member_index, const auto& member_name>()
 			{
 				constexpr auto member_id = engine::hash(member_name).value();
 				
