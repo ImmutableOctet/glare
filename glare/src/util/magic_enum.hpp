4614e7a5567a05e874f0da6822cb09a4563ba29d
Anthony Diamond <sonickid8@live.com>
2024-02-17 01:48:43 -0500

    Added include for `magic_enum_utility`.

--- glare/src/util/magic_enum.hpp
+++ glare/src/util/magic_enum.hpp
@@ -3,5 +3,5 @@
 #define MAGIC_ENUM_RANGE_MIN 0 // -128
 #define MAGIC_ENUM_RANGE_MAX 2048 // 1024 // 512 // 32767 // 65535 // 256 // 128
 
-//#include <magic_enum/magic_enum.hpp>
 #include <magic_enum.hpp>
+#include <magic_enum_utility.hpp>
