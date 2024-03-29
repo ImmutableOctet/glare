e057e77d0e17c6cb03fb7c7513c74db665d7dc57
Anthony Diamond <sonickid8@live.com>
2024-03-29 02:12:49 -0400

    Expanded `util:api` preprocessor checks.

--- glare/src/util/api.hpp
+++ glare/src/util/api.hpp
@@ -1,10 +1,21 @@
 #pragma once
 
-#define GLARE_EXPORT __declspec(dllexport)
-#define GLARE_IMPORT __declspec(dllimport)
-
-#ifdef GLARE_API_EXPORT_SYMBOLS
-	#define GLARE_API GLARE_EXPORT
+#ifdef _WIN32
+	#define GLARE_IMPORT __declspec(dllimport)
+	#define GLARE_EXPORT __declspec(dllexport)
 #else
-	#define GLARE_API GLARE_IMPORT
+	#define GLARE_IMPORT
+	#define GLARE_EXPORT
+#endif
+
+#ifdef GLARE_UTIL_EXPORT_SYMBOLS
+	#define GLARE_UTIL_API GLARE_EXPORT
+#else
+	#define GLARE_UTIL_API GLARE_IMPORT
+#endif
+
+#ifdef GLARE_SCRIPT_EXPORT_SYMBOLS
+	#define GLARE_SCRIPT_API GLARE_EXPORT
+#else
+	#define GLARE_SCRIPT_API GLARE_IMPORT
 #endif
