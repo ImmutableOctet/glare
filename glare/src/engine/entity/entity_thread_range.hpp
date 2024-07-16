546b9d9209b721bfc18bb91333a876ed4fd733b3
Anthony Diamond <sonickid8@live.com>
2024-07-16 16:37:45 -0400

    Modified `EntityThreadRange` to formally allow for an invalid state. (Uses new thread index constants) -- This behavior is needed now that a thread is not required to have a registered index in the descriptor.

--- glare/src/engine/entity/entity_thread_range.hpp
+++ glare/src/engine/entity/entity_thread_range.hpp
@@ -6,8 +6,8 @@ namespace engine
 {
 	struct EntityThreadRange
 	{
-		EntityThreadIndex start_index;
-		EntityThreadCount thread_count;
+		EntityThreadIndex start_index = ENTITY_THREAD_INDEX_INVALID;
+		EntityThreadCount thread_count = {};
 
 		inline EntityThreadIndex begin_point() const
 		{
@@ -16,17 +16,37 @@ namespace engine
 
 		inline EntityThreadIndex end_point() const
 		{
+			if (is_invalid())
+			{
+				return (start_index);
+			}
+
 			return (start_index + thread_count);
+		}
+
+		inline bool is_invalid() const
+		{
+			return (start_index == ENTITY_THREAD_INDEX_INVALID);
 		}
 
 		inline std::size_t size() const
 		{
+			if (is_invalid())
+			{
+				return 0;
+			}
+
 			return static_cast<std::size_t>(thread_count);
 		}
 
 		inline bool empty() const
 		{
 			return (size() == 0);
+		}
+
+		inline explicit operator bool() const
+		{
+			return (!empty());
 		}
 
 		bool operator==(const EntityThreadRange&) const noexcept = default;
