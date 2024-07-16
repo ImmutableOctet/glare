c396cdc8010e01566ec9bae098fd249813aaaf8a
Anthony Diamond <sonickid8@live.com>
2024-07-16 15:36:13 -0400

    Reworked automated `reflect_all` implementation to be more in-line with strategy used by generated code.

--- glare/src/engine/meta/static_variables.cpp
+++ glare/src/engine/meta/static_variables.cpp
@@ -5,26 +5,20 @@
 #include <string_view>
 #include <unordered_map>
 
-#define GLARE_ENGINE_REFLECT_ALL_AUTOMATICALLY
-
-#ifdef GLARE_ENGINE_REFLECT_ALL_AUTOMATICALLY
+#if GLARE_ENGINE_REFLECT_ALL_AUTOMATICALLY
 	#include "reflect_all.hpp"
-#endif
+#endif // GLARE_ENGINE_REFLECT_ALL_AUTOMATICALLY
 
 namespace engine
 {
 	namespace impl
 	{
-		std::unordered_map<StringHash, std::variant<std::string, std::string_view>> known_string_hashes;
-
-		// NOTE: This is defined in the same file as `known_string_hashes` to avoid out-of-order initialization issues.
-#ifdef GLARE_ENGINE_REFLECT_ALL_AUTOMATICALLY
-        struct reflect_all_t
-        {
-            reflect_all_t() { engine::reflect_all(); }
-        };
-
-		static reflect_all_t execute_reflect_all {};
-#endif
+#if GLARE_ENGINE_REFLECT_ALL_AUTOMATICALLY
+		reflect_all_t::reflect_all_t() { engine::reflect_all(); }
+#endif // GLARE_ENGINE_REFLECT_ALL_AUTOMATICALLY
 	}
 }
+
+#if GLARE_ENGINE_REFLECT_ALL_AUTOMATICALLY
+	const struct GLARE_ENGINE_SYMBOL engine::impl::reflect_all_t glare_impl_engine_execute_reflect_all {};
+#endif // GLARE_ENGINE_REFLECT_ALL_AUTOMATICALLY
