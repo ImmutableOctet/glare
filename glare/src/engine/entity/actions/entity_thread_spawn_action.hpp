28ba8da2d2e190d49f1d7fbb966086d211b10b82
Anthony Diamond <sonickid8@live.com>
2024-07-16 12:21:25 -0400

    Added `parent_thread_name` to `EntityThreadSpawnAction`.

--- glare/src/engine/entity/actions/entity_thread_spawn_action.hpp
+++ glare/src/engine/entity/actions/entity_thread_spawn_action.hpp
@@ -8,6 +8,8 @@ namespace engine
 	{
 		EntityThreadTarget threads;
 
+		EntityThreadID parent_thread_name = {};
+
 		bool restart_existing = false;
 
 		bool operator==(const EntityThreadSpawnAction&) const noexcept = default;
