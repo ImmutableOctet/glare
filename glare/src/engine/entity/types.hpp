ec48e6607b0cabc2af02de480ae9c48053a2d07b
Anthony Diamond <sonickid8@live.com>
2024-07-16 15:47:28 -0400

    Added constants for invalid indices.

--- glare/src/engine/entity/types.hpp
+++ glare/src/engine/entity/types.hpp
@@ -37,4 +37,8 @@ namespace engine
 		EntityStateIndex index;
 		std::optional<EntityStateHash> id; // EntityStateHash
 	};
+
+	inline constexpr EntityThreadIndex      ENTITY_THREAD_INDEX_INVALID      = static_cast<EntityThreadIndex>(-1ull);
+	inline constexpr EntityInstructionIndex ENTITY_INSTRUCTION_INDEX_INVALID = static_cast<EntityInstructionIndex>(-1ull);
+	inline constexpr EntityStateIndex       ENTITY_STATE_INDEX_INVALID       = static_cast<EntityStateIndex>(-1ull);
 }
