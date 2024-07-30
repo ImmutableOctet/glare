cd7b743e11aedd17566aca45c199e4547598b05e
Anthony Diamond <sonickid8@live.com>
2024-07-30 12:03:25 -0400

    * Added `duration_cast` alias * Implemented `per_second` command * Changed default/system clock type to `std::chrono::steady_clock` * Expanded `tick_rate` API to support arbitrary frames per-second as input

--- glare/src/engine/time_decl.hpp
+++ glare/src/engine/time_decl.hpp
@@ -10,16 +10,16 @@ namespace engine
 	{
 		namespace shared
 		{
-			using Days           = std::chrono::days;
-			using Hours          = std::chrono::hours;
-			using Minutes        = std::chrono::minutes;
-			using Seconds        = std::chrono::seconds; 
-			using Milliseconds   = std::chrono::milliseconds;
-			using Microseconds   = std::chrono::microseconds;
-			using Nanoseconds    = std::chrono::nanoseconds;
+			using Days          = std::chrono::days;
+			using Hours         = std::chrono::hours;
+			using Minutes       = std::chrono::minutes;
+			using Seconds       = std::chrono::seconds;
+			using Milliseconds  = std::chrono::milliseconds;
+			using Microseconds  = std::chrono::microseconds;
+			using Nanoseconds   = std::chrono::nanoseconds;
 
-			using FloatSeconds   = std::chrono::duration<float>;
-			using DoubleSeconds  = std::chrono::duration<double>;
+			using FloatSeconds  = std::chrono::duration<float>;
+			using DoubleSeconds = std::chrono::duration<double>;
 
 			template <typename RepType>
 			using CustomDurationType = std::chrono::duration<RepType>;
@@ -27,12 +27,14 @@ namespace engine
 			template <typename ClockType>
 			using CustomTimePoint = std::chrono::time_point<ClockType>;
 
+			using std::chrono::duration_cast;
+
 			namespace impl
 			{
 				template <typename OutputDurationType, typename InputDurationType>
 				constexpr OutputDurationType duration_cast(InputDurationType&& duration_value)
 				{
-					return std::chrono::duration_cast<OutputDurationType>(duration_value);
+					return std::chrono::duration_cast<OutputDurationType>(std::forward<InputDurationType>(duration_value));
 				}
 
 				template <typename OutputDurationType, typename SecondsType>
@@ -64,20 +66,48 @@ namespace engine
 				{
 					return to_duration<OutputDurationType>(DoubleSeconds(seconds));
 				}
+
+				// Divides one second (converted to nanoseconds) by the `denominator` specified.
+				constexpr auto second_divide(auto denominator)
+				{
+					return
+					(
+						duration_cast<Nanoseconds>(Seconds { 1 })
+						/
+						denominator
+					);
+				}
+
+				template <typename OutputDurationType, typename FramesPerSecondType>
+				constexpr OutputDurationType tick_rate(FramesPerSecondType&& frames_per_second)
+				{
+					return duration_cast<OutputDurationType>
+					(
+						second_divide(std::forward<FramesPerSecondType>(frames_per_second))
+					);
+				}
+			}
+
+			// Converts the specified rate (fraction of a second) into nanoseconds,
+			// then scales it inversely relative to one full second (converted to nanoseconds).
+			// 
+			// The result is the number of times `rate` can happen in a second.
+			constexpr auto per_second(auto&& rate)
+			{
+				return engine::time::shared::impl::second_divide(engine::time::shared::duration_cast<Nanoseconds>(rate));
 			}
 		}
+	}
 
+	namespace time
+	{
 		using namespace engine::time::shared;
 
-		using Clock          = std::chrono::system_clock; // std::chrono::high_resolution_clock;
-		using TimePoint      = CustomTimePoint<Clock>; // Clock::time_point;
-		using Duration       = Clock::duration;
-		using DurationRaw    = Duration::rep;
-
-		using Interval       = Duration;
-		using UpdateInterval = Interval;
-		using Rate           = Duration;
-		using UpdateRate     = Rate;
+		using Clock        = std::chrono::steady_clock; // std::chrono::system_clock; // std::chrono::high_resolution_clock;
+		using TimePoint    = Clock::time_point; // CustomTimePoint<Clock>;
+		using Duration     = Clock::duration;
+		using DurationRaw  = Duration::rep;
+		using Interval     = Duration;
 
 		namespace impl
 		{
@@ -93,21 +123,30 @@ namespace engine
 		constexpr Duration to_duration(float seconds)         { return engine::time::shared::impl::to_duration<Duration>(seconds); }
 		constexpr Duration to_duration(double seconds)        { return engine::time::shared::impl::to_duration<Duration>(seconds); }
 
-		constexpr Interval tick_rate()
+		constexpr auto tick_rate(auto frames_per_second)
 		{
 			//return to_duration(engine::TARGET_FRAME_DELTA_EX);
 
-			return engine::time::impl::duration_cast
-			(
-				std::chrono::duration_cast<Nanoseconds>(Seconds { 1 })
-				/
-				TARGET_FRAMES_PER_SECOND
-			);
+			return engine::time::shared::impl::tick_rate<Duration>(frames_per_second);
+		}
+
+		constexpr auto tick_rate()
+		{
+			return tick_rate(TARGET_FRAMES_PER_SECOND);
 		}
 	}
 
 	using SystemClock    = time::Clock;
 	using TimeDuration   = time::Duration;
 	using TimeInterval   = time::Interval;
-	using UpdateInterval = time::UpdateInterval;
+	using TimePoint      = time::TimePoint;
+	using UpdateInterval = time::Interval;
+
+	using Days           = time::shared::Days;
+	using Hours          = time::shared::Hours;
+	using Minutes        = time::shared::Minutes;
+	using Seconds        = time::shared::Seconds; 
+	using Milliseconds   = time::shared::Milliseconds;
+	using Microseconds   = time::shared::Microseconds;
+	using Nanoseconds    = time::shared::Nanoseconds;
 }
